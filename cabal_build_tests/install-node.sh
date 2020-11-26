43d10c540cef2e3ce995d3efbff0caddf2543777
James Browning <james.browning@iohk.io>
2020-11-26 10:42:50 +0000

    Improved scripts based on ShellCheck feedback

--- cabal_build_tests/install-node.sh
+++ cabal_build_tests/install-node.sh
@@ -6,13 +6,13 @@
 
 echo ""
 
-if [[$TAGGED_VERSION == "null" ]]
+if [[ $TAGGED_VERSION == "null" ]]
 then
 	printf "Please specify TAGGED_VERSION on docker run.\ne.g. '-e TAGGED_VERSION=1.23.0'"
 fi
 
 echo "Using tagged version $TAGGED_VERSION"
-cd ~
+cd ~ || exit 1
 
 # Install dependencies
 echo "Install dependencies"
@@ -29,7 +29,7 @@ then
 	apt-get install automake build-essential pkg-config libffi-dev libgmp-dev libssl-dev libtinfo-dev libsystemd-dev zlib1g-dev make g++ tmux git jq wget libncursesw5 libtool autoconf -y
 else
 	echo "/etc/os-relase does not contain 'fedora' or 'ubuntu'"
-	echo "$(cat /etc/os-release)"
+	cat /etc/os-release
 	exit 1
 fi
 
@@ -47,7 +47,7 @@ then
 	#echo "export PATH=\"~/.local/bin:\$PATH\"" >> ~/.bashrc
 	#echo "END OF BASHRC IS $(tail -n1 ~/.bashrc)" #REMOVE
 	#bash -c 'source ~/.bashrc' # CANT USE source IN THIS SCRIPT BECAUSE DOCKER RUNS THIS AS SUBSCRIPT
-	export PATH="~/.local/bin:$PATH"
+	export PATH=~/.local/bin:"$PATH"
 else
 	echo "$PATH already contains ~/.local/bin"
 fi
@@ -68,16 +68,16 @@ echo "Download and install GHC"
 wget https://downloads.haskell.org/ghc/8.10.2/ghc-8.10.2-x86_64-deb9-linux.tar.xz
 tar -xf ghc-8.10.2-x86_64-deb9-linux.tar.xz
 rm ghc-8.10.2-x86_64-deb9-linux.tar.xz
-cd ghc-8.10.2
+cd ghc-8.10.2 || exit 1
 ./configure
 make install
 
-cd ~
+cd ~ || exit 1
 
 # Install Libsodium
 echo "Install Lobsodium"
 git clone https://github.com/input-output-hk/libsodium
-cd libsodium
+cd libsodium || exit 1
 git checkout 66f017f1
 ./autogen.sh
 ./configure
@@ -89,9 +89,9 @@ export PKG_CONFIG_PATH="/usr/local/lib/pkgconfig:$PKG_CONFIG_PATH"
 
 # Download the source code for cardano-node
 echo "Download the source code for cardano-node"
-cd ~
+cd ~ || exit 1
 git clone https://github.com/input-output-hk/cardano-node.git
-cd cardano-node
+cd cardano-node || exit 1
 git fetch --all --tags
 
 if [[ $(git tag) != *"$TAGGED_VERSION"* ]]
@@ -100,22 +100,25 @@ then
 	exit 1
 fi
 
-git checkout tags/$TAGGED_VERSION
+git checkout "tags/$TAGGED_VERSION"
 
 # Build and install the node
 echo "Build and install the node"
 cabal build all
-cp -p dist-newstyle/build/x86_64-linux/ghc-8.10.2/cardano-node-$TAGGED_VERSION/x/cardano-node/build/cardano-node/cardano-node ~/.local/bin/
-cp -p dist-newstyle/build/x86_64-linux/ghc-8.10.2/cardano-cli-$TAGGED_VERSION/x/cardano-cli/build/cardano-cli/cardano-cli ~/.local/bin/
+cp -p "dist-newstyle/build/x86_64-linux/ghc-8.10.2/cardano-node-$TAGGED_VERSION/x/cardano-node/build/cardano-node/cardano-node" ~/.local/bin/
+cp -p "dist-newstyle/build/x86_64-linux/ghc-8.10.2/cardano-cli-$TAGGED_VERSION/x/cardano-cli/build/cardano-cli/cardano-cli" ~/.local/bin/
 
 # Verify node is installed
 echo "Verify node is installed"
 if [[ $(cardano-cli --version) == *"$TAGGED_VERSION"* ]]
 then
-	echo "$(cardano-cli --version)"
+	cardano-cli --version
 else
 	echo "$(cardano-cli --version) does not contain $TAGGED_VERSION"
 	exit 1
 fi
 
-printf "\nSuccess\n"
+printf "\nSuccess\n\n"
+echo "You can use 'docker exec -it <container-id> /bin/bash' in a separate session to play in the environment."
+echo "Press any key to stop container."
+read
--- cabal_build_tests/run-fedora.sh
+++ cabal_build_tests/run-fedora.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker build . -f fedora/Dockerfile -t cardano-node-fedora && \
-	docker run -it -e TAGGED_VERSION=$1 cardano-node-fedora
+	docker run --security-opt label:disable -it -e TAGGED_VERSION=$1 cardano-node-fedora
