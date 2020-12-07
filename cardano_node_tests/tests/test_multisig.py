b7d0fe32fd0ff1fb1b915168b12589323908f243
Martin Kourim <martin.kourim@iohk.io>
2020-12-07 23:08:57 +0100

    Tests for auxiliary scripts

--- cardano_node_tests/tests/test_multisig.py
+++ cardano_node_tests/tests/test_multisig.py
@@ -1,4 +1,10 @@
-"""Tests for multisig transactions."""
+"""Tests for multisig transactions and scripts.
+
+* multisig
+* time locking
+* auxiliary scripts
+"""
+import json
 import logging
 import random
 from pathlib import Path
@@ -6,6 +12,7 @@ from typing import List
 from typing import Optional
 
 import allure
+import cbor2
 import pytest
 from _pytest.tmpdir import TempdirFactory
 from packaging import version
@@ -17,6 +24,8 @@ from cardano_node_tests.utils import helpers
 from cardano_node_tests.utils import parallel_run
 
 LOGGER = logging.getLogger(__name__)
+
+DATA_DIR = Path(__file__).parent / "data"
 
 
 @pytest.fixture(scope="module")
@@ -991,3 +1000,186 @@ class TestTimeLocking:
                 script_is_src=True,
             )
         assert "ScriptWitnessNotValidatingUTXOW" in str(excinfo.value)
+
+
+@pytest.mark.skipif(
+    cluster_instances.TX_ERA == "shelley" or helpers.NODE_VERSION < version.parse("1.24.0"),
+    reason="runs on version >= 1.24.0 and with Allegra+ TX",
+)
+class TestAuxiliaryScripts:
+    """Tests for auxiliary scripts."""
+
+    JSON_METADATA_FILE = DATA_DIR / "tx_metadata.json"
+    CBOR_METADATA_FILE = DATA_DIR / "tx_metadata.cbor"
+
+    @pytest.fixture
+    def payment_addrs(
+        self,
+        cluster_manager: parallel_run.ClusterManager,
+        cluster: clusterlib.ClusterLib,
+    ) -> List[clusterlib.AddressRecord]:
+        """Create new payment addresses."""
+        data_key = id(TestAuxiliaryScripts)
+        cached_value = cluster_manager.cache.test_data.get(data_key)
+        if cached_value:
+            return cached_value  # type: ignore
+
+        addrs = clusterlib_utils.create_payment_addr_records(
+            *[
+                f"multi_addr_aux_scripts_ci{cluster_manager.cluster_instance}_{i}"
+                for i in range(20)
+            ],
+            cluster_obj=cluster,
+        )
+        cluster_manager.cache.test_data[data_key] = addrs
+
+        # fund source addresses
+        clusterlib_utils.fund_from_faucet(
+            addrs[0],
+            cluster_obj=cluster,
+            faucet_data=cluster_manager.cache.addrs_data["user1"],
+            amount=20_000_000,
+        )
+
+        return addrs
+
+    @allure.link(helpers.get_vcs_link())
+    def test_tx_script_metadata_json(
+        self, cluster: clusterlib.ClusterLib, payment_addrs: List[clusterlib.AddressRecord]
+    ):
+        """Send transaction with auxiliary script and metadata JSON.
+
+        Check that the auxiliary script is present in the TX body.
+        """
+        temp_template = helpers.get_func_name()
+
+        payment_vkey_files = [p.vkey_file for p in payment_addrs]
+
+        # create multisig script
+        multisig_script = cluster.build_multisig_script(
+            script_name=temp_template,
+            script_type_arg=clusterlib.MultiSigTypeArgs.ALL,
+            payment_vkey_files=payment_vkey_files,
+            slot=10,
+            slot_type_arg=clusterlib.MultiSlotTypeArgs.AFTER,
+        )
+
+        tx_files = clusterlib.TxFiles(
+            signing_key_files=[payment_addrs[0].skey_file],
+            metadata_json_files=[self.JSON_METADATA_FILE],
+            script_files=[multisig_script],
+        )
+        tx_raw_output = cluster.send_tx(
+            src_address=payment_addrs[0].address, tx_name=temp_template, tx_files=tx_files
+        )
+        cluster.wait_for_new_block(new_blocks=2)
+        assert tx_raw_output.fee, "Transaction had no fee"
+
+        with open(tx_raw_output.out_file) as body_fp:
+            tx_body_json = json.load(body_fp)
+
+        cbor_body = bytes.fromhex(tx_body_json["cborHex"])
+        cbor_body_metadata = cbor2.loads(cbor_body)[1]
+
+        cbor_body_script = cbor_body_metadata[1]
+        assert cbor_body_script, "Auxiliary script not present"
+
+    @allure.link(helpers.get_vcs_link())
+    def test_tx_script_metadata_cbor(
+        self, cluster: clusterlib.ClusterLib, payment_addrs: List[clusterlib.AddressRecord]
+    ):
+        """Send transaction with auxiliary script and metadata CBOR.
+
+        Check that the auxiliary script is present in the TX body.
+        """
+        temp_template = helpers.get_func_name()
+
+        payment_vkey_files = [p.vkey_file for p in payment_addrs]
+
+        # create multisig script
+        multisig_script = cluster.build_multisig_script(
+            script_name=temp_template,
+            script_type_arg=clusterlib.MultiSigTypeArgs.AT_LEAST,
+            payment_vkey_files=payment_vkey_files,
+            slot=1000,
+            slot_type_arg=clusterlib.MultiSlotTypeArgs.BEFORE,
+        )
+
+        tx_files = clusterlib.TxFiles(
+            signing_key_files=[payment_addrs[0].skey_file],
+            metadata_cbor_files=[self.CBOR_METADATA_FILE],
+            script_files=[multisig_script],
+        )
+        tx_raw_output = cluster.send_tx(
+            src_address=payment_addrs[0].address, tx_name=temp_template, tx_files=tx_files
+        )
+        cluster.wait_for_new_block(new_blocks=2)
+        assert tx_raw_output.fee, "Transaction had no fee"
+
+        with open(tx_raw_output.out_file) as body_fp:
+            tx_body_json = json.load(body_fp)
+
+        cbor_body = bytes.fromhex(tx_body_json["cborHex"])
+        cbor_body_metadata = cbor2.loads(cbor_body)[1]
+
+        cbor_body_script = cbor_body_metadata[1]
+        assert cbor_body_script, "Auxiliary script not present"
+
+    @allure.link(helpers.get_vcs_link())
+    def test_tx_script_no_metadata(
+        self, cluster: clusterlib.ClusterLib, payment_addrs: List[clusterlib.AddressRecord]
+    ):
+        """Send transaction with auxiliary script and no other metadata.
+
+        Check that the auxiliary script is present in the TX body.
+        """
+        temp_template = helpers.get_func_name()
+
+        payment_vkey_files = [p.vkey_file for p in payment_addrs]
+
+        # create multisig script
+        multisig_script = cluster.build_multisig_script(
+            script_name=temp_template,
+            script_type_arg=clusterlib.MultiSigTypeArgs.ANY,
+            payment_vkey_files=payment_vkey_files,
+        )
+
+        tx_files = clusterlib.TxFiles(
+            signing_key_files=[payment_addrs[0].skey_file],
+            script_files=[multisig_script],
+        )
+        tx_raw_output = cluster.send_tx(
+            src_address=payment_addrs[0].address, tx_name=temp_template, tx_files=tx_files
+        )
+        cluster.wait_for_new_block(new_blocks=2)
+        assert tx_raw_output.fee, "Transaction had no fee"
+
+        with open(tx_raw_output.out_file) as body_fp:
+            tx_body_json = json.load(body_fp)
+
+        cbor_body = bytes.fromhex(tx_body_json["cborHex"])
+        cbor_body_metadata = cbor2.loads(cbor_body)[1]
+
+        cbor_body_script = cbor_body_metadata[1]
+        assert cbor_body_script, "Auxiliary script not present"
+
+    @allure.link(helpers.get_vcs_link())
+    def test_tx_script_invalid(
+        self, cluster: clusterlib.ClusterLib, payment_addrs: List[clusterlib.AddressRecord]
+    ):
+        """Build transaction with invalid auxiliary script.
+
+        Expect failure.
+        """
+        temp_template = helpers.get_func_name()
+
+        tx_files = clusterlib.TxFiles(
+            signing_key_files=[payment_addrs[0].skey_file],
+            script_files=[self.JSON_METADATA_FILE],  # not valid script file
+        )
+
+        with pytest.raises(clusterlib.CLIError) as excinfo:
+            cluster.send_tx(
+                src_address=payment_addrs[0].address, tx_name=temp_template, tx_files=tx_files
+            )
+        assert 'Error in $: key "type" not found' in str(excinfo.value)
