c29402ef0fcad9027ee795787b43fb504dcbb44b
Martin Kourim <martin.kourim@iohk.io>
2021-04-29 15:51:58 +0200

    Update python requirements

--- nix/cardano-clusterlib.nix
+++ nix/cardano-clusterlib.nix
@@ -2,10 +2,10 @@
 
 buildPythonPackage rec {
   pname = "cardano-clusterlib";
-  version = "0.1.13";
+  version = "0.1.14";
   src = fetchPypi {
     inherit pname version;
-    sha256 = "07h96vpmyh3bc6s5dlqgsz4gpsfzkd91xn7wvs154176237hhgx7";
+    sha256 = "15ii1x5lasfckvikc0phhv78jsajbdq3lyhpjcvkk9bs16iv7wkc";
   };
   doCheck = false;
   nativeBuildInputs = [ setuptools_scm ];
