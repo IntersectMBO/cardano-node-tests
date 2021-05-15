892e7b5727e0204fee9a800a7cc7842cf6e3ff22
Martin Kourim <martin.kourim@iohk.io>
2021-05-15 23:31:07 +0200

    Copy the start cluster script command log

--- cardano_node_tests/utils/cli_coverage.py
+++ cardano_node_tests/utils/cli_coverage.py
@@ -1,6 +1,7 @@
 """Functionality for CLI coverage data collected by the `clusterlib`."""
 import json
 import logging
+import shutil
 from pathlib import Path
 from typing import Optional
 
@@ -21,5 +22,19 @@ def save_cli_coverage(cluster_obj: clusterlib.ClusterLib, pytest_config: Config)
     json_file = Path(cli_coverage_dir) / f"cli_coverage_{helpers.get_timestamped_rand_str()}.json"
     with open(json_file, "w") as out_json:
         json.dump(cluster_obj.cli_coverage, out_json, indent=4)
-    LOGGER.info(f"Coverage files saved to '{cli_coverage_dir}'.")
+    LOGGER.info(f"Coverage file saved to '{cli_coverage_dir}'.")
     return json_file
+
+
+def save_start_script_coverage(log_file: Path, pytest_config: Config) -> Optional[Path]:
+    """Save info about CLI commands executed by cluster start script."""
+    cli_coverage_dir = pytest_config.getoption("--cli-coverage-dir")
+    if not (cli_coverage_dir and log_file.exists()):
+        return None
+
+    dest_file = (
+        Path(cli_coverage_dir) / f"cli_coverage_script_{helpers.get_timestamped_rand_str()}.log"
+    )
+    shutil.copy(log_file, dest_file)
+    LOGGER.info(f"Start script coverage log file saved to '{dest_file}'.")
+    return dest_file
