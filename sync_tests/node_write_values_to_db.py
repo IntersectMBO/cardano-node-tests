54a52ecf5ba7856a100e50b3d322e0a728dcc2ae
dorin100 <Bica123!>
2021-05-28 11:50:55 +0200

    added support for reading all qa automated tests - node & db-sync

--- sync_tests/node_write_values_to_db.py
+++ sync_tests/node_write_values_to_db.py
@@ -1,157 +1,15 @@
 import ast
 import json
 import os
-import sqlite3
 from collections import OrderedDict
-from sqlite3 import Error
 from pathlib import Path
 import argparse
-import pandas as pd
+
+from sqlite_utils import get_column_names_from_table, add_column_to_table, get_last_row_no, \
+    add_test_values_into_db, export_db_table_to_csv
 
 DATABASE_NAME = r"node_sync_tests_results.db"
 RESULTS_FILE_NAME = r"sync_results.json"
-
-
-def create_connection(db_file):
-    conn = None
-    try:
-        conn = sqlite3.connect(db_file)
-        return conn
-    except Error as e:
-        print(f"!!! Error connecting to the database:\n {e}")
-
-    return conn
-
-
-def add_test_values_into_db(table_name, col_names_list, col_values_list):
-    current_directory = Path.cwd()
-    database_path = Path(current_directory) / DATABASE_NAME
-    print(f"  -- database_path: {database_path}")
-
-    col_names = ','.join(col_names_list)
-    col_spaces = ','.join(['?'] * len(col_names_list))
-    conn = create_connection(database_path)
-    sql_query = f"INSERT INTO {table_name} (%s) values(%s)" % (col_names, col_spaces)
-    print(f"  -- sql_query: {sql_query}")
-    try:
-        cur = conn.cursor()
-        cur.execute(sql_query, col_values_list)
-        conn.commit()
-        cur.close()
-    except sqlite3.Error as error:
-        print(f"  -- !!! ERROR: Failed to insert data into {table_name} table:\n", error)
-        return False
-    finally:
-        if conn:
-            conn.close()
-    return True
-
-
-def export_db_table_to_csv(table_name):
-    print(f"Exporting {table_name} table into CSV file")
-    current_directory = Path.cwd()
-
-    database_path = Path(current_directory) / DATABASE_NAME
-    csv_files_path = Path(current_directory) / "csv_files"
-
-    print(f"  -- database_path : {database_path}")
-    print(f"  -- csv_files_path: {csv_files_path}")
-
-    Path(csv_files_path).mkdir(parents=True, exist_ok=True)
-
-    conn = create_connection(database_path)
-    sql_query = f"select * from {table_name}"
-    print(f"  -- sql_query: {sql_query}")
-    try:
-        cur = conn.cursor()
-        cur.execute(sql_query)
-
-        with open(csv_files_path / f"{table_name}.csv", "w") as csv_file:
-            df = pd.read_sql(f"select * from {table_name}", conn)
-            df.to_csv(csv_file, escapechar="\n", index=False)
-
-        conn.commit()
-        cur.close()
-
-        print(f"  -- Data exported Successfully into {csv_files_path / f'{table_name}.csv'}")
-    except sqlite3.Error as error:
-        print(f"!!! ERROR: Failed to export {table_name} table to CSV:\n", error)
-        return False
-    finally:
-        if conn:
-            conn.close()
-    return True
-
-
-def get_column_names_from_table(env):
-    table_name = env
-    print(f"Getting the column names from {table_name} table")
-    current_directory = Path.cwd()
-    print(f"current_directory: {current_directory}")
-
-    database_path = Path(current_directory) / DATABASE_NAME
-    print(f"  -- database_path: {database_path}")
-    conn = create_connection(database_path)
-    sql_query = f"select * from {table_name}"
-    print(f"  -- sql_query: {sql_query}")
-    try:
-        cur = conn.cursor()
-        cur.execute(sql_query)
-        col_name_list = [res[0] for res in cur.description]
-        return col_name_list
-    except sqlite3.Error as error:
-        print(f"!!! ERROR: Failed to get column names from {table_name} table:\n", error)
-        return False
-    finally:
-        if conn:
-            conn.close()
-
-
-def get_last_row_no(table_name):
-    print(f"Getting the last row no from {table_name} table")
-    current_directory = Path.cwd()
-    print(f"  -- current_directory: {current_directory}")
-
-    database_path = Path(current_directory) / DATABASE_NAME
-    print(f"  -- database_path: {database_path}")
-    conn = create_connection(database_path)
-
-    sql_query = f"SELECT count(*) FROM {table_name};"
-    print(f"  -- sql_query: {sql_query}")
-    try:
-        cur = conn.cursor()
-        cur.execute(sql_query)
-        last_row_no = cur.fetchone()[0]
-        return last_row_no
-    except sqlite3.Error as error:
-        print(f"!!! ERROR: Failed to get the last row no from {table_name} table:\n", error)
-        return False
-    finally:
-        if conn:
-            conn.close()
-
-
-def add_column_to_table(env, column_name, column_type):
-    table_name = env
-    print(f"Adding column {column_name} with type {column_type} to {table_name} table")
-    current_directory = Path.cwd()
-
-    database_path = Path(current_directory) / DATABASE_NAME
-    print(f"  -- database_path: {database_path}")
-    conn = create_connection(database_path)
-    sql_query = f"alter table {table_name} add column {column_name} {column_type}"
-    print(f"  -- sql_query: {sql_query}")
-    try:
-        cur = conn.cursor()
-        cur.execute(sql_query)
-        col_name_list = [res[0] for res in cur.description]
-        return col_name_list
-    except sqlite3.Error as error:
-        print(f"!!! ERROR: Failed to add {column_name} column into {table_name} table:\n", error)
-        return False
-    finally:
-        if conn:
-            conn.close()
 
 
 def main():
@@ -170,13 +28,15 @@ def main():
 
     current_directory = Path.cwd()
     print(f"current_directory: {current_directory}")
-    print(f" - sync_tests listdir: {os.listdir(current_directory)}")
+    print(f" - listdir: {os.listdir(current_directory)}")
 
     print("  ==== Move to 'sync_tests' directory")
     os.chdir(current_directory / "sync_tests")
     current_directory = Path.cwd()
     print(f"current_directory: {current_directory}")
     print(f" - sync_tests listdir: {os.listdir(current_directory)}")
+    database_path = Path(current_directory) / DATABASE_NAME
+    print(f"database_path: {database_path}")
 
     print("  ==== Check if there are DB columns for all the eras")
 
@@ -186,7 +46,7 @@ def main():
     print(f"eras_in_test: {eras_in_test}")
 
     print(f"Get the column names inside the {env} DB tables")
-    table_column_names = get_column_names_from_table(env)
+    table_column_names = get_column_names_from_table(database_path, env)
     print(f"  -- table_column_names: {table_column_names}")
 
     for era in eras_in_test:
@@ -200,9 +60,9 @@ def main():
                                 str(era + "_end_sync_time"),
                                 str(era + "_sync_duration_secs")]
             for column_name in new_columns_list:
-                add_column_to_table(env, column_name, "TEXT")
+                add_column_to_table(database_path, env, column_name, "TEXT")
 
-    sync_test_results_dict["identifier"] = sync_test_results_dict["env"] + "_" + str(get_last_row_no(env))
+    sync_test_results_dict["identifier"] = sync_test_results_dict["env"] + "_" + str(get_last_row_no(database_path, env))
 
     print(f"  ==== Write test values into the {env + '_logs_table'} DB table")
     log_values_json = ast.literal_eval(str((sync_test_results_dict["log_values"])))
@@ -217,7 +77,7 @@ def main():
 
         col_list2 = list(line_dict.keys())
         col_values2 = list(line_dict.values())
-        if not add_test_values_into_db(env + "_logs", col_list2, col_values2):
+        if not add_test_values_into_db(database_path, env + "_logs", col_list2, col_values2):
             print(f"col_list2  : {col_list2}")
             print(f"col_values2: {col_values2}")
             exit(1)
@@ -235,7 +95,7 @@ def main():
 
         col_list3 = list(sync_duration_per_epoch_dict.keys())
         col_values3 = list(sync_duration_per_epoch_dict.values())
-        if not add_test_values_into_db(env + "_epoch_duration", col_list3, col_values3):
+        if not add_test_values_into_db(database_path, env + "_epoch_duration", col_list3, col_values3):
             print(f"col_list3  : {col_list3}")
             print(f"col_values3: {col_values3}")
             exit(1)
@@ -246,19 +106,19 @@ def main():
 
     col_list = list(sync_test_results_dict.keys())
     col_values = list(sync_test_results_dict.values())
-    if not add_test_values_into_db(env, col_list, col_values):
+    if not add_test_values_into_db(database_path, env, col_list, col_values):
         print(f"col_list  : {col_list}")
         print(f"col_values: {col_values}")
         exit(1)
 
     print(f"  ==== Exporting the {env} table as CSV")
-    export_db_table_to_csv(env)
+    export_db_table_to_csv(database_path, env)
 
     print(f"  ==== Exporting the {env + '_logs'} table as CSV")
-    export_db_table_to_csv(env + '_logs')
+    export_db_table_to_csv(database_path, env + '_logs')
 
     print(f"  ==== Exporting the {env + '_epoch_duration'} table as CSV")
-    export_db_table_to_csv(env + '_epoch_duration')
+    export_db_table_to_csv(database_path, env + '_epoch_duration')
 
 
 if __name__ == "__main__":
---
+++ sync_tests/results_write_values_to_db.py
@@ -0,0 +1,158 @@
+import ast
+import json
+import os
+from collections import OrderedDict
+from datetime import datetime
+from pathlib import Path
+import argparse
+
+import requests
+
+from sqlite_utils import add_test_values_into_db, get_column_values, export_db_table_to_csv
+from utils import seconds_to_time, date_diff_in_seconds
+
+DATABASE_NAME = r"automated_tests_results.db"
+ORG_SLUG = "input-output-hk"
+cli_envs = ["node_cli", "dbsync_cli"]
+nightly_envs = ["node_nightly", "dbsync_nightly"]
+
+
+def get_buildkite_pipeline_builds(buildkite_token, pipeline_slug):
+    url = f"https://api.buildkite.com/v2/organizations/{ORG_SLUG}/pipelines/{pipeline_slug}/builds"
+    headers = {'Authorization': "Bearer " + buildkite_token}
+    print(f"url: {url}")
+    response = requests.get(url, headers=headers)
+
+    status_code = response.status_code
+    if status_code == 200:
+        return response.json()
+    else:
+        print(f"status_code: {status_code}")
+        print(f"response   : {response.json()}")
+        print(f"!!! ERROR: status_code =! 200 when getting the builds for {pipeline_slug} pipeline")
+        exit(1)
+
+
+def get_buildkite_pipeline_slug(env):
+    pileline_slug = None
+    if env == "node_cli":
+        pileline_slug = "cardano-node-tests"
+    elif env == "node_nightly":
+        pileline_slug = "cardano-node-tests-nightly"
+    elif env == "dbsync_cli":
+        pileline_slug = "cardano-node-tests-dbsync"
+    elif env == "dbsync_nightly":
+        pileline_slug = "cardano-node-tests-nightly-dbsync"
+    else:
+        print(f"!!! ERROR: env {env} not expected - use one of: {cli_envs + nightly_envs}")
+        exit(1)
+    return pileline_slug
+
+
+def main():
+    secret = vars(args)["secret"]
+
+    current_directory = Path.cwd()
+    print(f"current_directory: {current_directory}")
+    # print("  ==== Move to 'sync_tests' directory")
+    # os.chdir(current_directory / "sync_tests")
+    # current_directory = Path.cwd()
+    print(f"current_directory: {current_directory}")
+
+    database_path = Path(current_directory) / DATABASE_NAME
+
+    for env in cli_envs:
+        print(f" === env: {env}")
+        pileline_slug = get_buildkite_pipeline_slug(env)
+        pipeline_builds = get_buildkite_pipeline_builds(secret, pileline_slug)
+
+        print(f" - there are {len(pipeline_builds)} builds")
+        print("Adding build results into the DB")
+        build_results_dict = OrderedDict()
+        for build in pipeline_builds:
+            # don't add the same build no twice
+            if build["number"] not in get_column_values(database_path, env, "build_no"):
+                build_results_dict["build_no"] = build["number"]
+                build_results_dict["build_id"] = build["id"]
+                build_results_dict["build_web_url"] = build["web_url"]
+                build_results_dict["build_status"] = build["state"]
+                build_results_dict["build_started_at"] = build["started_at"]
+                build_results_dict["build_finished_at"] = build["finished_at"]
+                build_results_dict["build_duration"] = seconds_to_time(date_diff_in_seconds(
+                    datetime.strptime(build_results_dict["build_finished_at"], "%Y-%m-%dT%H:%M:%S.%fZ"),
+                    datetime.strptime(build_results_dict["build_started_at"], "%Y-%m-%dT%H:%M:%S.%fZ")))
+                build_results_dict["test_branch"] = build["branch"]
+                build_results_dict["node_branch"] = build["env"]["NODE_BRANCH"]
+                build_results_dict["node_rev"] = build["env"]["NODE_REV"]
+                build_results_dict["cluster_era"] = build["env"]["CLUSTER_ERA"]
+                build_results_dict["tx_era"] = build["env"]["TX_ERA"]
+                if "dbsync" in env:
+                    build_results_dict["dbsync_branch"] = build["env"]["DBSYNC_BRANCH"]
+                    build_results_dict["dbsync_rev"] = build["env"]["DBSYNC_REV"]
+
+                print(f"  ==== Writing test results into the {env} DB table for build no {build['number']}")
+                col_list = list(build_results_dict.keys())
+                col_values = list(build_results_dict.values())
+
+                if not add_test_values_into_db(database_path, env, col_list, col_values):
+                    print(f"col_list  : {col_list}")
+                    print(f"col_values: {col_values}")
+                    exit(1)
+            else:
+                print(f" -- results for build {build['number']} are already into the DB")
+
+        print(f"  ==== Exporting the {env} table as CSV")
+        export_db_table_to_csv(database_path, env)
+
+    for env in nightly_envs:
+        print(f" === env: {env}")
+        pileline_slug = get_buildkite_pipeline_slug(env)
+        pipeline_builds = get_buildkite_pipeline_builds(secret, pileline_slug)
+
+        print(f" - there are {len(pipeline_builds)} builds")
+        print("Adding build results into the DB")
+        build_results_dict = OrderedDict()
+        for build in pipeline_builds:
+            # don't add the same build no twice
+            if (env == "dbsync_nightly") & (build["number"] < 47):
+                continue
+            if build["number"] not in get_column_values(database_path, env, "build_no"):
+                build_results_dict["build_no"] = build["number"]
+                build_results_dict["build_id"] = build["id"]
+                build_results_dict["build_web_url"] = build["web_url"]
+                build_results_dict["build_status"] = build["state"]
+                build_results_dict["build_started_at"] = build["started_at"]
+                build_results_dict["build_finished_at"] = build["finished_at"]
+                build_results_dict["build_duration"] = seconds_to_time(date_diff_in_seconds(
+                    datetime.strptime(build_results_dict["build_finished_at"], "%Y-%m-%dT%H:%M:%S.%fZ"),
+                    datetime.strptime(build_results_dict["build_started_at"], "%Y-%m-%dT%H:%M:%S.%fZ")))
+                build_results_dict["test_branch"] = build["branch"]
+                build_results_dict["node_branch"] = "master"
+                if "dbsync" in env:
+                    build_results_dict["dbsync_branch"] = "master"
+
+                print(f"  ==== Writing test results into the {env} DB table for build no {build['number']}")
+                col_list = list(build_results_dict.keys())
+                col_values = list(build_results_dict.values())
+
+                if not add_test_values_into_db(database_path, env, col_list, col_values):
+                    print(f"col_list  : {col_list}")
+                    print(f"col_values: {col_values}")
+                    exit(1)
+            else:
+                print(f" -- results for build {build['number']} are already into the DB")
+
+        print(f"  ==== Exporting the {env} table as CSV")
+        export_db_table_to_csv(database_path, env)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Get node cli nightly results\n\n")
+
+    parser.add_argument(
+        "-t", "--secret", help="buildkite secret"
+    )
+
+    args = parser.parse_args()
+
+    main()
