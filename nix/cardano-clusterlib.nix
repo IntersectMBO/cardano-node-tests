68359519ca0c4f503369683b3fc2329cc396d8a2
Martin Kourim <martin.kourim@iohk.io>
2021-07-12 14:01:00 +0200

    Update plutus test to work with node master

--- nix/cardano-clusterlib.nix
+++ nix/cardano-clusterlib.nix
@@ -2,10 +2,10 @@
 
 buildPythonPackage rec {
   pname = "cardano-clusterlib";
-  version = "0.1.26";
+  version = "0.1.27";
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1qh101dxh1mbbri5vfnllzhwibp4gldfnmpamacxx6h70ihansrj";
+    sha256 = "1b39kywrhb8i7n7dwckpa6svvg7z4im85rbwrhf7a07ghdia7l61";
   };
   doCheck = false;
   nativeBuildInputs = [ setuptools_scm ];
