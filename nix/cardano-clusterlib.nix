0e5d8338f426436ffdfb722088df9ef55834db61
Martin Kourim <martin.kourim@iohk.io>
2021-08-06 13:27:35 +0200

    Use new version of cardano-clusterlib

--- nix/cardano-clusterlib.nix
+++ nix/cardano-clusterlib.nix
@@ -2,10 +2,10 @@
 
 buildPythonPackage rec {
   pname = "cardano-clusterlib";
-  version = "0.1.32";
+  version = "0.1.34";
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0ll16969vrscjr5fzks5lfvy96g9b1j5dfhkax6dfx4v5gbhaib3";
+    sha256 = "1w4lcc8bbzvqhjbv83a9k38xqvr52gdlszwihwgmf0hlrw7xvwrw";
   };
   doCheck = false;
   nativeBuildInputs = [ setuptools_scm ];
