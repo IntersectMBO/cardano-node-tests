d714380eabb9f40839dcfecc2c42032999ad1be3
Martin Kourim <martin.kourim@iohk.io>
2021-08-27 22:21:48 +0200

    Remove workaround with `tomli` dependency
    It broke nix-shell and is no longer needed.

--- nix/cardano-clusterlib.nix
+++ nix/cardano-clusterlib.nix
@@ -2,10 +2,10 @@
 
 buildPythonPackage rec {
   pname = "cardano-clusterlib";
-  version = "0.1.37";
+  version = "0.1.38";
   src = fetchPypi {
     inherit pname version;
-    sha256 = "08shsp4l31sqqpjpf5lx4hvvrh81nsscgl0c5njyqh54qx64phbh";
+    sha256 = "036f96ax5kjl6zhkaw2jlk6j9n6xlbpkp9rp17haj6b33cai5bsb";
   };
   doCheck = false;
   nativeBuildInputs = [ setuptools_scm ];
