6ea691704c20277c981eadf45088f8a9627969b5
dorin100 <Bica123!>
2021-09-21 18:27:45 +0300

    improved the logging

--- sync_tests/node_sync_test.py
+++ sync_tests/node_sync_test.py
@@ -935,7 +935,7 @@ def main():
     print(f"Write the test values to the {current_directory / RESULTS_FILE_NAME} file")
     with open(RESULTS_FILE_NAME, 'w') as results_file:
         json.dump(test_values_dict, results_file, indent=2)
-    print(f"test_values_dict: {test_values_dict}\n")
+    print(f"test_values_dict items: {test_values_dict.items}\n")
 
 
 if __name__ == "__main__":
