4f6572a66ad7e957565aac4d59e834c5a8b0672c
Martin Kourim <martin.kourim@iohk.io>
2021-09-30 00:24:03 +0200

    Update cardano-clusterlib

--- nix/cardano-clusterlib.nix
+++ nix/cardano-clusterlib.nix
@@ -2,10 +2,10 @@
 
 buildPythonPackage rec {
   pname = "cardano-clusterlib";
-  version = "0.1.44";
+  version = "0.1.45";
   src = fetchPypi {
     inherit pname version;
-    sha256 = "06m9iffhxn37qjyp95ar9rw6hvz4gr78p3bnm5jyramjdshwrbcw";
+    sha256 = "063842hjch27313fjk97dh5fpn712ifn3k860gzllp1b3w66mbdp";
   };
   doCheck = false;
   nativeBuildInputs = [ setuptools_scm ];
