89a0412089dabd7ad81407d57dfd46588b1142f1
Martin Kourim <martin.kourim@iohk.io>
2021-10-14 15:55:23 +0200

    Simplify python venv setup

---
+++ setup_venv.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+set -eu
+
+if [ -n "${IN_NIX_SHELL:-""}" ]; then
+  echo "This script is not supposed to run inside nix shell". >&2
+  exit 1
+fi
+if [ ! -d "cardano_node_tests" ]; then
+  echo "This script is supposed to run from the root of cardano-node-tests directory". >&2
+  exit 1
+fi
+if [ -n "${VIRTUAL_ENV:-""}" ]; then
+  echo "A python virtual env is already activated in this shell. To install cardano-node-tests, run \`make install\`". >&2
+  exit 1
+fi
+
+# create a python virtual env if it doesn't exist
+if [ ! -e ".env" ]; then
+  python3 -m venv .env
+fi
+
+# activate the virtual env
+# shellcheck disable=SC1091
+. .env/bin/activate
+
+# install cardano-node-tests and its dependencies into the virtual env
+make install
