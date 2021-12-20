f823fc6d8c1cabd7bf3db6810ec4734cea2c1b47
Martin Kourim <martin.kourim@iohk.io>
2021-12-20 21:45:06 +0100

    Include file name in the log message

--- cardano_node_tests/tests/common.py
+++ cardano_node_tests/tests/common.py
@@ -48,12 +48,20 @@ def get_test_id(cluster_obj: clusterlib.ClusterLib) -> str:
 
     Log the test ID into cluster manager log file.
     """
-    func_name = inspect.currentframe().f_back.f_code.co_name  # type: ignore
+    calling_frame = inspect.currentframe().f_back  # type: ignore
+    func_name = calling_frame.f_code.co_name  # type: ignore
     rand_str = clusterlib.get_rand_str(3)
     test_id = f"{func_name}_ci{cluster_obj.cluster_id}_{rand_str}"
 
-    # log to cluster manager log file - getting test ID happens at the beginning of a test,
-    # so the log entry can be used for determining when the test started
+    # log test ID to cluster manager log file - getting test ID happens early
+    # after the start of a test, so the log entry can be used for determining
+    # time of the test start
+    i_self = calling_frame.f_locals.get("self")  # type: ignore
+    if i_self:
+        code_id = f"{i_self.__class__.__module__}.{i_self.__class__.__qualname__}"
+    else:
+        code_id = calling_frame.f_globals["__name__"]  # type: ignore
     cm: cluster_management.ClusterManager = cluster_obj._cluster_manager  # type: ignore
-    cm._log(f"c{cm.cluster_instance_num}: got ID `{test_id}` for test function `{func_name}`")
+    cm._log(f"c{cm.cluster_instance_num}: got ID `{test_id}` for `{code_id}.{func_name}`")
+
     return test_id
