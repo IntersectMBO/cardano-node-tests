78966f98a55f624bdf65f65d45cef9b8765190c8
Martin Kourim <martin.kourim@iohk.io>
2021-12-20 13:39:52 +0100

    Fix quotes

--- cardano_node_tests/tests/common.py
+++ cardano_node_tests/tests/common.py
@@ -12,7 +12,7 @@ BUILD_USABLE = (
     VERSIONS.transaction_era >= VERSIONS.MARY and VERSIONS.transaction_era == VERSIONS.cluster_era
 )
 BUILD_SKIP_MSG = (
-    f"cannot use `build` with cluster era '{VERSIONS.cluster_era_name}` "
+    f"cannot use `build` with cluster era '{VERSIONS.cluster_era_name}' "
     f"and TX era '{VERSIONS.transaction_era_name}'"
 )
 
