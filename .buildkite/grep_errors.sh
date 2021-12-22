0a9cc6d409ae112bafc7e1430b2a04ca13f0ebf7
Martin Kourim <martin.kourim@iohk.io>
2021-12-22 11:47:04 +0100

    Save testing artifacts on buildkite CI jobs

--- .buildkite/grep_errors.sh
+++ .buildkite/grep_errors.sh
@@ -3,7 +3,8 @@
 ARTIFACTS_DIR="${ARTIFACTS_DIR:-".artifacts"}"
 LOGFILE="$PWD/errors_all.log"
 
-# there should be only one dir matching "pytest-*" in the artifacts dir
-pushd "$ARTIFACTS_DIR"/pytest-* || { echo "Cannot switch to $ARTIFACTS_DIR/pytest-*"; ls -1a "$ARTIFACTS_DIR"; exit 1; } > "$LOGFILE"
+# shellcheck disable=SC2012
+latest_artifacts="$(ls -1td --quoting-style=shell-escape "$ARTIFACTS_DIR"/pytest-* | head -1)"
+pushd "$latest_artifacts" || { echo "Cannot switch to $latest_artifacts"; ls -1a "$ARTIFACTS_DIR"; exit 1; } > "$LOGFILE"
 grep -r --include "*.stdout" --include "*.stderr" -Ei ":error:|failed|failure" . > "$LOGFILE"
 popd || exit 1
