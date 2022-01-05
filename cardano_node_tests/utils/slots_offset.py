898c08703a0b83f89aef6013aff9669a363753f9
Martin Kourim <martin.kourim@iohk.io>
2022-01-05 17:05:49 +0100

    Refactor slots offset

--- cardano_node_tests/utils/slots_offset.py
+++ cardano_node_tests/utils/slots_offset.py
@@ -12,7 +12,11 @@ def _datetime2timestamp(datetime_str: str) -> int:
 
 
 def get_slots_offset(genesis_byron: Path, genesis_shelley: Path, shelley_start: str = "") -> int:
-    """Get offset of slots from Byron configuration vs current era configuration.
+    """Get offset of slots between Byron and Shelley eras.
+
+    Byron and Shelley eras can have different epoch and slot lengths. The slots offset is needed
+    in order to be able to calculate what slot number to expect in certain point of time
+    (e.g. epoch boundary).
 
     When no value for `shelley_start` is provided, assume it's local cluster where Shelley
     era starts after a single Byron epoch.
@@ -22,33 +26,30 @@ def get_slots_offset(genesis_byron: Path, genesis_shelley: Path, shelley_start: 
     with open(genesis_shelley, encoding="utf-8") as in_json:
         shelley_dict = json.load(in_json)
 
-    slot_duration_byron_msec = int(byron_dict["blockVersionData"]["slotDuration"])
-    slot_duration_byron = float(slot_duration_byron_msec / 1000)
-    slot_duration_shelley = float(shelley_dict["slotLength"])
+    slot_length_byron_msec = int(byron_dict["blockVersionData"]["slotDuration"])
+    # slot lengths in seconds
+    slot_length_byron = float(slot_length_byron_msec / 1000)
+    slot_length_shelley = float(shelley_dict["slotLength"])
 
     if shelley_start:
-        start_timestamp: int = byron_dict["startTime"]
-        testnet_timestamp = _datetime2timestamp(shelley_start)
-        offset_sec = testnet_timestamp - start_timestamp
+        start_timestamp = int(byron_dict["startTime"])
+        shelley_timestamp = _datetime2timestamp(shelley_start)
+        timestamp_offset = shelley_timestamp - start_timestamp
 
         # assume that epoch length is the same for Byron and Shelley epochs
-        slots_in_byron = int(offset_sec / slot_duration_byron)
-        slots_in_shelley = int(offset_sec / slot_duration_shelley)
+        slots_in_byron = int(timestamp_offset / slot_length_byron)
+        slots_in_shelley = int(timestamp_offset / slot_length_shelley)
     else:
-        slots_per_epoch_shelley = int(shelley_dict["epochLength"])
         byron_k = int(byron_dict["protocolConsts"]["k"])
+        byron_epoch_length = int(byron_k * 10 * slot_length_byron)
 
-        byron_epoch_sec = int(byron_k * 10 * slot_duration_byron)
-        shelley_epoch_sec = int(slots_per_epoch_shelley * slot_duration_shelley)
+        slots_per_epoch_shelley = int(shelley_dict["epochLength"])
+        shelley_epoch_length = int(slots_per_epoch_shelley * slot_length_shelley)
 
-        if (slot_duration_byron == slot_duration_shelley) and (
-            byron_epoch_sec == shelley_epoch_sec
-        ):
-            return 0
-
-        # assume that Shelley era starts at epoch 1, i.e. after a single Byron epoch
-        slots_in_byron = int(byron_epoch_sec / slot_duration_byron)
-        slots_in_shelley = int(shelley_epoch_sec / slot_duration_shelley)
+        # Assume that Shelley era starts at epoch 1, i.e. after a single Byron epoch.
+        # Epoch length can differ between Byron and Shelley epochs.
+        slots_in_byron = int(byron_epoch_length / slot_length_byron)
+        slots_in_shelley = int(shelley_epoch_length / slot_length_shelley)
 
     offset = slots_in_shelley - slots_in_byron
 
