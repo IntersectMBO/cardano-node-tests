d02fe4109ec3f581c96238b7ab071710a4b97651
Martin Kourim <martin.kourim@iohk.io>
2022-01-27 16:38:15 +0100

    Fix gh actions broken by commit 428f6a9

--- .buildkite/node_sync_tests_pull.sh
+++ .buildkite/node_sync_tests_pull.sh
@@ -3,6 +3,7 @@
 : "${sshkey:=/run/keys/buildkite-ssh-iohk-devops-private}"
 echo "Authenticating push using SSH with $sshkey"
 export GIT_SSH_COMMAND="ssh -i $sshkey -F /dev/null"
+# shellcheck disable=SC2034
 remote="git@github.com:input-output-hk/cardano-node-tests.git"
 
 git fetch origin
