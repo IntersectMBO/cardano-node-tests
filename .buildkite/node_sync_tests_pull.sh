IntersectMBO/cardano-node-tests

commit 428f6a96087f3da36ed7decfb53d30875715229f

updated the CI pipelines on master too (to be used on the "sync tests" branch)

.buildkite/node_sync_tests_pull.sh
@@ -1,0 +1,9 @@
+#!/usr/bin/env bash
+
+: "${sshkey:=/run/keys/buildkite-ssh-iohk-devops-private}"
+echo "Authenticating push using SSH with $sshkey"
+export GIT_SSH_COMMAND="ssh -i $sshkey -F /dev/null"
+remote="git@github.com:input-output-hk/cardano-node-tests.git"
+
+git fetch origin
+git merge origin/sync_tests

.buildkite/nightly_dbsync.sh
@@ -1,77 +1,59 @@
 #! /usr/bin/env nix-shell
-#! nix-shell -i bash -p niv nix gnugrep gnumake gnutar coreutils adoptopenjdk-jre-bin curl git xz
+#! nix-shell -i bash -p niv nix gnumake gnutar coreutils adoptopenjdk-jre-bin curl git
 #! nix-shell -I nixpkgs=./nix
 # shellcheck shell=bash
 
 set -xeuo pipefail
 
 REPODIR="$PWD"
-export ARTIFACTS_DIR="${ARTIFACTS_DIR:-".artifacts"}"
-export CLUSTERS_COUNT="${CLUSTERS_COUNT:-5}"
 
 WORKDIR="/scratch/workdir"
 rm -rf "$WORKDIR"
 mkdir -p "$WORKDIR"
 
-# update cardano-node to specified branch and/or revision, or to the latest available
-# shellcheck disable=SC1090,SC1091
-. "$REPODIR/.buildkite/niv_update_cardano_node.sh"
+# update to latest cardano-node
+niv update
 
 pushd "$WORKDIR"
 
 # install Allure
-# shellcheck disable=SC1090,SC1091
-. "$REPODIR/.buildkite/allure_install.sh"
+mkdir allure_install
+curl -sfLo \
+  allure_install/allure.tgz \
+  "https://repo.maven.apache.org/maven2/io/qameta/allure/allure-commandline/2.13.9/allure-commandline-2.13.9.tgz"
+tar xzf allure_install/allure.tgz -C allure_install
+export PATH="$PWD/allure_install/allure-2.13.9/bin:$PATH"
 
-# clone db-sync
-git clone git@github.com:input-output-hk/cardano-db-sync.git
+# build dbsync
+git clone --depth 1 git@github.com:input-output-hk/cardano-db-sync.git
 pushd cardano-db-sync
-if [ -n "${DBSYNC_REV:-""}" ]; then
-  git fetch
-  git checkout "$DBSYNC_REV"
-elif [ -n "${DBSYNC_BRANCH:-""}" ]; then
-  git fetch
-  git checkout "$DBSYNC_BRANCH"
-else
-  git pull origin master
-fi
-git rev-parse HEAD
-
-# build db-sync
 nix-build -A cardano-db-sync -o db-sync-node
 export DBSYNC_REPO="$PWD"
+popd
 
-pushd "$REPODIR"
+cd "$REPODIR"
 
 # set postgres env variables
 export PGHOST=localhost
 export PGUSER=postgres
 export PGPORT=5432
+export PGPASSFILE="$WORKDIR/postgres/pgpass"
 
 # start and setup postgres
 ./scripts/postgres-start.sh "$WORKDIR/postgres" -k
 
+# setup dbsync
+./scripts/postgres-setup.sh --createdb
+
 # run tests and generate report
-rm -rf "${ARTIFACTS_DIR:?}"/*
 set +e
 # shellcheck disable=SC2016
 nix-shell --run \
-  'SCHEDULING_LOG=scheduling.log CARDANO_NODE_SOCKET_PATH="$CARDANO_NODE_SOCKET_PATH_CI" CI_ARGS="-m dbsync --html=testrun-report.html --self-contained-html" make tests; retval="$?"; ./.buildkite/report.sh .; exit "$retval"'
+  'SCHEDULING_LOG=scheduling.log CARDANO_NODE_SOCKET_PATH="$CARDANO_NODE_SOCKET_PATH_CI" TEST_THREADS=5 CLUSTERS_COUNT=1 FORBID_RESTART=1 CI_ARGS="-m dbsync --html=nightly-report.html --self-contained-html" make tests; retval="$?"; ./.buildkite/report.sh .; exit "$retval"'
 retval="$?"
-
-# grep testing artifacts for errors
-# shellcheck disable=SC1090,SC1091
-. "$REPODIR/.buildkite/grep_errors.sh"
-
-# save testing artifacts
-# shellcheck disable=SC1090,SC1091
-. "$REPODIR/.buildkite/save_artifacts.sh"
-
-# compress scheduling log
-xz scheduling.log
 
 echo
 echo "Dir content:"
-ls -1a
+ls -1
 
 exit "$retval"

.buildkite/nightly_dbsync.yml
@@ -9,13 +9,7 @@ steps:
     agents:
       system: x86_64-linux
     artifact_paths:
-      - 'allure-report.tar.xz'
+      - 'allure-report.tar.bz2'
       - 'cli_coverage.json'
-      - 'testrun-report.html'
-      - 'scheduling.log.xz'
-      - 'errors_all.log'
-      - 'testing_artifacts.tar.xz'
-
-notify:
-  - email: "cardano_qa_node_db_sy-aaaadudb2xnnewjhenny4cgzia@input-output-rnd.slack.com"
-    if: build.state != "passed"
+      - 'nightly-report.html'
+      - 'scheduling.log'

.buildkite/node_sync_tests.yml
@@ -1,0 +1,14 @@
+steps:
+  - label: 'run the cardano node sync test on Mainnet using a Linux machine'
+    commands:
+      - nix-shell --run 'python ./sync_tests/node_sync_test.py -t1 $tag_no1 -t2 $tag_no2 -e "mainnet" -e1 $hydra_eval_no1 -e2 $hydra_eval_no2'
+      - './.buildkite/node_sync_tests_pull.sh'
+      - nix-shell --run 'python ./sync_tests/node_write_sync_values_to_db.py -e "mainnet"'
+    branches: "sync_tests"
+    timeout_in_minutes: 5000
+    agents:
+      system: x86_64-linux
+      queue: benchmark
+    artifact_paths:
+      - 'logfile.log'
+      - 'sync_results.json'

.github/workflows/nightly_tests_results.yaml
@@ -1,4 +1,4 @@
-name: get automated test results
+name: save the automated tests runs status to database
 
 on:
   schedule:
@@ -15,7 +15,6 @@ jobs:
         uses: actions/checkout@v2
         with:
           path: cardano_node_tests
-          ref: sync_tests
       - name: run actions/setup-python@v2
         uses: actions/setup-python@v2
         with:
@@ -24,27 +23,23 @@ jobs:
         run: |
           pip install requests
           pip install psutil
-          pip install sqlite-utils>=2.18
-          sqlite-utils --version
           pip install pandas
       - name: checkout sync_tests repo
         run: |
           cd cardano_node_tests
           git config --global user.name "sync_tests"
           git config --global user.email "action@github.com"
+
           git checkout sync_tests
           echo "current branch1: $(git branch --show-current)"
       - name: get the automated tests results
+        env:
+          BUILDKITE_API_ACCESS_TOKEN: ${{ secrets.BUILDKITE_API_ACCESS_TOKEN }}
+          AWS_DB_USERNAME: ${{ secrets.AWS_DB_USERNAME }}
+          AWS_DB_PASS: ${{ secrets.AWS_DB_PASS }}
+          AWS_DB_NAME: ${{ secrets.AWS_DB_NAME }}
+          AWS_DB_HOSTNAME: ${{ secrets.AWS_DB_HOSTNAME }}
         run: |
           cd cardano_node_tests
           echo "current branch2: $(git branch --show-current)"
-          python sync_tests/results_write_values_to_db.py -t ${{ secrets.BUILDKITE_API_ACCESS_TOKEN }}
-      - name: commit and push changes
-        run: |
-          cd cardano_node_tests
-          echo "current branch3: $(git branch --show-current)"
-          git status
-          git add sync_tests/automated_tests_results.db
-          git add sync_tests/csv_files
-          git commit -m "added automated test results"
-          git push origin HEAD:sync_tests --force
+          python sync_tests/nightly_tests_write_values_to_db.py

.github/workflows/node_mainnet_tx_count_per_epoch.yaml
@@ -1,0 +1,45 @@
+name: add into the db the mainnet number of transactions per epoch
+
+on:
+  schedule:
+    - cron:  '0 8 * * *'
+  workflow_dispatch:
+    branches:
+      - sync_tests
+
+jobs:
+  cli_nightly_last_run:
+    runs-on: ubuntu-latest
+    steps:
+      - name: checkout cardano-node-tests repo
+        uses: actions/checkout@v2
+        with:
+          path: cardano_node_tests
+      - name: run actions/setup-python@v2
+        uses: actions/setup-python@v2
+        with:
+          python-version: 3.8
+      - name: install dependencies
+        run: |
+          pip install requests
+          pip install psutil
+          pip install pandas
+      - name: checkout sync_tests repo
+        run: |
+          cd cardano_node_tests
+          git config --global user.name "sync_tests"
+          git config --global user.email "action@github.com"
+
+          git checkout sync_tests
+          echo "current branch1: $(git branch --show-current)"
+      - name: get the automated tests results
+        env:
+          BUILDKITE_API_ACCESS_TOKEN: ${{ secrets.BUILDKITE_API_ACCESS_TOKEN }}
+          AWS_DB_USERNAME: ${{ secrets.AWS_DB_USERNAME }}
+          AWS_DB_PASS: ${{ secrets.AWS_DB_PASS }}
+          AWS_DB_NAME: ${{ secrets.AWS_DB_NAME }}
+          AWS_DB_HOSTNAME: ${{ secrets.AWS_DB_HOSTNAME }}
+        run: |
+          cd cardano_node_tests
+          echo "current branch2: $(git branch --show-current)"
+          python sync_tests/node_write_mainnet_tx_count_per_epoch.py

.github/workflows/node_sync_test.yaml
@@ -12,12 +12,12 @@ on:
         description: tag number2 - used for final sync, from existing state
         required: false
         default: "None"
-      db_sync_tag_no1:
-        description: db_sync tag number1 - used for initial sync, from clean state
+      hydra_eval_no1:
+        description: hydra eval no - used for getting node prebuilt files
         required: false
         default: "None"
-      db_sync_tag_no2:
-        description: db_sync tag number2 - used for final sync, from existing state
+      hydra_eval_no2:
+        description: hydra eval no - used for getting node prebuilt files
         required: false
         default: "None"
 
@@ -34,8 +34,8 @@ jobs:
           env: '{
           "tag_no1":"${{ github.event.inputs.tag_no1 }}",
           "tag_no2":"${{ github.event.inputs.tag_no2 }}",
-          "dt_tag_no1":"${{ github.event.inputs.db_sync_tag_no1 }}",
-          "dt_tag_no2":"${{ github.event.inputs.db_sync_tag_no2 }}"
+          "hydra_eval_no1":"${{ github.event.inputs.hydra_eval_no1 }}",
+          "hydra_eval_no2":"${{ github.event.inputs.hydra_eval_no2 }}"
           }'
           branch: 'sync_tests'
   sync_test:
@@ -60,7 +60,6 @@ jobs:
           pip install requests
           pip install psutil
           pip install sqlite-utils>=2.18
-          sqlite-utils --version
           pip install pandas
       - name: checkout sync_tests repo
         run: |
@@ -73,18 +72,18 @@ jobs:
         run: |
           cd cardano_node_tests
           echo "current branch2: $(git branch --show-current)"
-          python sync_tests/node_sync_test.py -t1 "${{ github.event.inputs.tag_no1 }}" -t2 "${{ github.event.inputs.tag_no2 }}" -e ${{ matrix.env }}
-      - name: commit and push DB changes for the sync test
+          python sync_tests/node_sync_test.py -t1 "${{ github.event.inputs.tag_no1 }}" -t2 "${{ github.event.inputs.tag_no2 }}" -e1 "${{ github.event.inputs.hydra_eval_no1 }}" -e2 "${{ github.event.inputs.hydra_eval_no2 }}" -e ${{ matrix.env }}
+      - name: write the test results into the database
+        env:
+          BUILDKITE_API_ACCESS_TOKEN: ${{ secrets.BUILDKITE_API_ACCESS_TOKEN }}
+          AWS_DB_USERNAME: ${{ secrets.AWS_DB_USERNAME }}
+          AWS_DB_PASS: ${{ secrets.AWS_DB_PASS }}
+          AWS_DB_NAME: ${{ secrets.AWS_DB_NAME }}
+          AWS_DB_HOSTNAME: ${{ secrets.AWS_DB_HOSTNAME }}
         run: |
           cd cardano_node_tests
           echo "current branch3: $(git branch --show-current)"
-          git config pull.rebase true
-          git pull origin sync_tests
-          python sync_tests/node_write_values_to_db.py -e ${{ matrix.env }}
-          git add sync_tests/node_sync_tests_results.db
-          git add sync_tests/csv_files
-          git commit -m "added sync values for tag ${{ github.event.inputs.tag_no1 }} ${{ github.event.inputs.tag_no2 }} - ${{ matrix.env }} - ${{ matrix.os }}"
-          git push origin HEAD:sync_tests --force
+          python sync_tests/node_write_sync_values_to_db.py -e ${{ matrix.env }}
       - name: generate artifacts
         uses: actions/upload-artifact@v2
         if: always()

commit 7f932e731b76653a7f247f5203a6b718116a62cb

Fix gh actions broken by commit 428f6a9

.buildkite/node_sync_tests_pull.sh
@@ -3,6 +3,7 @@
 : "${sshkey:=/run/keys/buildkite-ssh-iohk-devops-private}"
 echo "Authenticating push using SSH with $sshkey"
 export GIT_SSH_COMMAND="ssh -i $sshkey -F /dev/null"
+# shellcheck disable=SC2034
 remote="git@github.com:input-output-hk/cardano-node-tests.git"
 
 git fetch origin