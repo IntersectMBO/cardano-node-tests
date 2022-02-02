2c1d09bb3c48344776004fb1a6bf95f3457155e9
Martin Kourim <martin.kourim@iohk.io>
2022-02-02 11:51:53 +0100

    Simplify fixtures for changing path to test file temp directory

--- cardano_node_tests/tests/test_blocks.py
+++ cardano_node_tests/tests/test_blocks.py
@@ -3,13 +3,11 @@
 Other block production checks may be present in `test_staking.py`.
 """
 import logging
-from pathlib import Path
 from typing import Dict
 from typing import List
 
 import allure
 import pytest
-from _pytest.tmpdir import TempdirFactory
 from cardano_clusterlib import clusterlib
 
 from cardano_node_tests.tests import common
@@ -22,22 +20,8 @@ LOGGER = logging.getLogger(__name__)
 
 
 @pytest.fixture(scope="module")
-def create_temp_dir(tmp_path_factory: TempdirFactory):
-    """Create a temporary dir."""
-    p = Path(tmp_path_factory.getbasetemp()).joinpath(helpers.get_id_for_mktemp(__file__)).resolve()
-    p.mkdir(exist_ok=True, parents=True)
-    return p
-
-
-@pytest.fixture
-def temp_dir(create_temp_dir: Path):
-    """Change to a temporary dir."""
-    with helpers.change_cwd(create_temp_dir):
-        yield create_temp_dir
-
-
-# use the "temp_dir" fixture for all tests automatically
-pytestmark = pytest.mark.usefixtures("temp_dir")
+def this_testfile():
+    return __file__
 
 
 @pytest.fixture
--- cardano_node_tests/tests/test_scripts.py
+++ cardano_node_tests/tests/test_scripts.py
@@ -12,7 +12,6 @@ from typing import Optional
 
 import allure
 import pytest
-from _pytest.tmpdir import TempdirFactory
 from cardano_clusterlib import clusterlib
 
 from cardano_node_tests.tests import common
@@ -29,22 +28,8 @@ DATA_DIR = Path(__file__).parent / "data"
 
 
 @pytest.fixture(scope="module")
-def create_temp_dir(tmp_path_factory: TempdirFactory):
-    """Create a temporary dir."""
-    p = Path(tmp_path_factory.getbasetemp()).joinpath(helpers.get_id_for_mktemp(__file__)).resolve()
-    p.mkdir(exist_ok=True, parents=True)
-    return p
-
-
-@pytest.fixture
-def temp_dir(create_temp_dir: Path):
-    """Change to a temporary dir."""
-    with helpers.change_cwd(create_temp_dir):
-        yield create_temp_dir
-
-
-# use the "temp_dir" fixture for all tests automatically
-pytestmark = pytest.mark.usefixtures("temp_dir")
+def this_testfile():
+    return __file__
 
 
 def multisig_tx(
