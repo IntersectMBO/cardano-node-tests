ecb5875e846cb868c3ddce588ac3cfb479c7bb7b
dorin100 <Bica123!>
2022-02-16 20:21:00 +0200

    installing python packages on Buildkite agent

--- .buildkite/node_sync_tests.sh
+++ .buildkite/node_sync_tests.sh
@@ -1,8 +1,7 @@
 #! /usr/bin/env nix-shell
-#! nix-shell -i bash -p python39Full python39Packages.virtualenv python39Packages.pip
+#! nix-shell -i bash -p python39Full python39Packages.pip
 #! nix-shell -I nixpkgs=./nix
 # shellcheck shell=bash
-
 
 pip install pymysql
 pip install requests
