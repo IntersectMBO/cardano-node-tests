2f4063ceb8d5fc01adaea8238fabb536aee8456f
dorin100 <Bica123!>
2022-02-18 10:01:00 +0200

    run the sync test from a python virtual env

--- .buildkite/node_sync_tests.sh
+++ .buildkite/node_sync_tests.sh
@@ -3,6 +3,17 @@
 #! nix-shell -I nixpkgs=./nix
 # shellcheck shell=bash
 
+echo "set WORKDIR"
+WORKDIR="/scratch/workdir"
+rm -rf "$WORKDIR"
+mkdir -p "$WORKDIR"
+
+echo "create and activate python virtual env"
+python3 -m venv "$WORKDIR/.env_sync"
+# shellcheck disable=SC1090,SC1091
+. "$WORKDIR/.env_sync/bin/activate"
+
+echo "install packages into python virtual env"
 pip install blockfrost-python
 
 tag_no1=$1
@@ -10,5 +21,8 @@ tag_no2=$2
 hydra_eval_no1=$3
 hydra_eval_no2=$4
 
+echo "start sync test"
 python ./sync_tests/node_sync_test.py -t1 "$tag_no1" -t2 "$tag_no2" -e "mainnet" -e1 "$hydra_eval_no1" -e2 "$hydra_eval_no2"
+
+echo "write sync test values into the db"
 python ./sync_tests/node_write_sync_values_to_db.py -e "mainnet"
