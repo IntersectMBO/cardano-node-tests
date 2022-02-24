fda5387153d337c6accd217aa3d33f4e15472e1b
Martin Kourim <martin.kourim@iohk.io>
2022-02-24 13:48:09 +0100

    Update required cardano-clusterlib version to 0.2.3

--- nix/cardano-clusterlib.nix
+++ nix/cardano-clusterlib.nix
@@ -2,10 +2,10 @@
 
 buildPythonPackage rec {
   pname = "cardano-clusterlib";
-  version = "0.2.2";
+  version = "0.2.3";
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sgf67AusQaB3S/WoFlLCIcfmU/kHHgcdZ5NuIg0OaBE=";
+    sha256 = "rR/s9p4KW9Upc1bo7a3Fy/GmI9m6U3LfrYXD+vAO/6U=";
   };
   doCheck = false;
   nativeBuildInputs = [ setuptools_scm ];
