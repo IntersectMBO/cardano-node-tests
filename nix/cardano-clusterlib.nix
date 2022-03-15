e9a24fa800bcf8960ba894fcbdfc7a26dc0eff8f
saratomaz <sara.tomaz@iohk.io>
2022-03-15 14:27:21 +0000

    update clusterlib version

--- nix/cardano-clusterlib.nix
+++ nix/cardano-clusterlib.nix
@@ -2,10 +2,10 @@
 
 buildPythonPackage rec {
   pname = "cardano-clusterlib";
-  version = "0.2.4";
+  version = "0.2.5";
   src = fetchPypi {
     inherit pname version;
-    sha256 = "JbpP8Kar8s11F/kZ7UsoxOkhMviqtkuSuBBHd9cDgks=";
+    sha256 = "TnVEWvbhnM3fKklXIeZrURV7mBk0D1wodAW6qIlkPRI=";
   };
   doCheck = false;
   nativeBuildInputs = [ setuptools_scm ];
