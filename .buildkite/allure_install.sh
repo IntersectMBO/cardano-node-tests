32ed86d7f62c69236fbf183fca8f58a6b7a69245
Martin Kourim <martin.kourim@iohk.io>
2022-05-03 12:08:35 +0200

    Download up-to-date allure

--- .buildkite/allure_install.sh
+++ .buildkite/allure_install.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+ALLURE_VERSION="2.17.3"
+
 # install Allure
 mkdir allure_install
 curl -sfLo \
   allure_install/allure.tgz \
-  "https://repo.maven.apache.org/maven2/io/qameta/allure/allure-commandline/2.13.9/allure-commandline-2.13.9.tgz"
+  "https://repo.maven.apache.org/maven2/io/qameta/allure/allure-commandline/${ALLURE_VERSION}/allure-commandline-${ALLURE_VERSION}.tgz"
 tar xzf allure_install/allure.tgz -C allure_install
-export PATH="$PWD/allure_install/allure-2.13.9/bin:$PATH"
+export PATH="$PWD/allure_install/allure-${ALLURE_VERSION}/bin:$PATH"
