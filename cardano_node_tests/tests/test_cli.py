d0f7eed0cbac39871eb13a5e00af98c4b62ee697
Martin Kourim <martin.kourim@iohk.io>
2022-05-07 00:29:35 +0200

    Compare with golden only on recent versions of node

--- cardano_node_tests/tests/test_cli.py
+++ cardano_node_tests/tests/test_cli.py
@@ -182,9 +182,10 @@ class TestCLI:
         tx_body = cluster.view_tx(tx_body_file=self.TX_BODY_FILE)
         tx = cluster.view_tx(tx_file=self.TX_FILE)
 
-        with open(self.TX_BODY_OUT, encoding="utf-8") as infile:
-            tx_body_view_out = infile.read()
-        assert tx_body == tx_body_view_out.strip()
+        if "payment credential key hash" in tx_body:
+            with open(self.TX_BODY_OUT, encoding="utf-8") as infile:
+                tx_body_view_out = infile.read()
+            assert tx_body == tx_body_view_out.strip()
 
         if "witnesses:" in tx:
             with open(self.TX_OUT, encoding="utf-8") as infile:
