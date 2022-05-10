02fa3670bb38605cb33f791d2d3d4d9ebb7503fc
Martin Kourim <martin.kourim@iohk.io>
2022-05-10 22:21:58 +0200

    Rework buildkite nightly jobs

--- .buildkite/save_artifacts.sh
+++ .buildkite/save_artifacts.sh
@@ -3,11 +3,9 @@
 ARTIFACTS_DIR="${ARTIFACTS_DIR:-".artifacts"}"
 ARTIFACTS_TAR="$PWD/testing_artifacts.tar.xz"
 
-pushd "$ARTIFACTS_DIR" || { echo "Cannot switch to $ARTIFACTS_DIR"; ls -1a; exit 1; }
-# shellcheck disable=SC2012
-latest_artifacts="$(ls -1td --quoting-style=shell-escape pytest-* | head -1)"
-popd || exit 1
+NEW_DIR="artifacts_$(date +%Y%m%d%H%M%S)"
+mv "$ARTIFACTS_DIR" "$NEW_DIR" || { echo "Cannot move $ARTIFACTS_DIR to $NEW_DIR"; ls -1a; exit 1; }
 
 echo "Creating artifacts archive $ARTIFACTS_TAR"
 rm -f "$ARTIFACTS_TAR"
-tar -C "$ARTIFACTS_DIR" -cJf "$ARTIFACTS_TAR" "$latest_artifacts"
+tar -cJf "$ARTIFACTS_TAR" "$NEW_DIR"
