e53a9d866907706eb2d7aaa2fdce41b82707a0f6
dorin100 <Bica123!>
2022-06-20 11:25:31 +0200

    added back the logs for the debug tracers

--- sync_tests/node_sync_test.py
+++ sync_tests/node_sync_test.py
@@ -346,19 +346,19 @@ def enable_cardano_node_tracers(node_config_filepath):
     print("  -- Set 'minSeverity' = 'Info'")
     node_config_json["minSeverity"] = "Info"
     # node_config_json["TestEnableDevelopmentNetworkProtocols"] = True
-    # node_config_json["TestEnableDevelopmentHardForkEras"] = True
+    node_config_json["TestEnableDevelopmentHardForkEras"] = True
 
-    # node_config_json["TraceLocalMux"] = True
-    # node_config_json["TraceLocalHandshake"] = True
-    # node_config_json["TraceLocalErrorPolicy"] = True
-    # node_config_json["TraceErrorPolicy"] = True
-    #
-    # if "mapSeverity" not in node_config_json["options"]:
-    #     node_config_json["options"]["mapSeverity"] = {}
-    # node_config_json["options"]["mapSeverity"]["cardano.node.LocalMux"] = "Info"
-    # node_config_json["options"]["mapSeverity"]["cardano.node.LocalHandshake"] = "Info"
-    # node_config_json["options"]["mapSeverity"]["cardano.node.LocalErrorPolicy"] = "Debug"
-    # node_config_json["options"]["mapSeverity"]["cardano.node.ErrorPolicy"] = "Debug"
+    node_config_json["TraceLocalMux"] = True
+    node_config_json["TraceLocalHandshake"] = True
+    node_config_json["TraceLocalErrorPolicy"] = True
+    node_config_json["TraceErrorPolicy"] = True
+
+    if "mapSeverity" not in node_config_json["options"]:
+        node_config_json["options"]["mapSeverity"] = {}
+    node_config_json["options"]["mapSeverity"]["cardano.node.LocalMux"] = "Info"
+    node_config_json["options"]["mapSeverity"]["cardano.node.LocalHandshake"] = "Info"
+    node_config_json["options"]["mapSeverity"]["cardano.node.LocalErrorPolicy"] = "Debug"
+    node_config_json["options"]["mapSeverity"]["cardano.node.ErrorPolicy"] = "Debug"
 
     print(json.dumps(node_config_json, indent=2))
 
