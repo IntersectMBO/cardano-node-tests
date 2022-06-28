fbd1fce88e5d90d04ff4a3791d13876b00349421
Joris Dral <jorisjdral@gmail.com>
2022-06-28 14:43:44 +0200

    Update cabal_build_tests scripts to allow testing builds at commits. (#1191)
    * Update cabal_build_tests scripts to allow testing builds at commits.
    
    The scripts were previously only aimed at build-testing `cardano-node`
    tags. The script can now also build-test specific commit hashes.
    
    Other changes:
    * Include installation of missing package dependencies (openssl, lmdb).
    * Include Secp256k1 installation.
    * "Parameterise" the script with cabal and ghc versions.
    * Installing of `cardano-cli` and `cardano-node` executables simplified.
    
    * Process PR comments: Missing Fedora packages, typos

--- cabal_build_tests/readme.md
+++ cabal_build_tests/readme.md
@@ -6,10 +6,14 @@
 ## Run tests
 
 ### To test cardano-node Cabal build on Ubuntu:
-`./run-ubuntu.sh <cardano-node tag>`
+`./run-ubuntu.sh -t tag -o <cardano-node tag>`
+
+`./run-ubuntu.sh -t commit -o <cardano-node commit>`
 
 ### To test cardano-node Cabal build on Fedora:
 
-`./run-fedora.sh <cardano-node tag>`
+`./run-fedora.sh  -t tag -o <cardano-node tag>`
+
+`./run-fedora.sh -t commit -o <cardano-node commit>`
 
 If "Success" is shown at the end and exit with code 0 then build has completed successfully and verified with cardano-cli.
--- cabal_build_tests/run-ubuntu.sh
+++ cabal_build_tests/run-ubuntu.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
+GIT_OBJECT_TYPE=""
+GIT_OBJECT=""
+
+while getopts t:o: flag
+do
+    case "${flag}" in
+        t) GIT_OBJECT_TYPE=${OPTARG};;
+		o) GIT_OBJECT=${OPTARG};;
+        *) echo "Error in command line parsing" >&2
+		   exit 1
+    esac
+done
+
 docker build . -f ubuntu/Dockerfile -t cardano-node-ubuntu && \
-	docker run --security-opt label:disable -it -e TAGGED_VERSION="$1" cardano-node-ubuntu
+	docker run --security-opt label:disable -it -e GIT_OBJECT_TYPE="$GIT_OBJECT_TYPE" -e GIT_OBJECT="$GIT_OBJECT" cardano-node-ubuntu
