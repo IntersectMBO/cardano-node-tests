3d824405f4c5d425ee842207b7bc12cbc37a16b8
saratomaz <sara.tomaz@iohk.io>
2022-06-30 15:43:27 +0100

    update check of kes-period-info command to return the error

--- cardano_node_tests/tests/kes.py
+++ cardano_node_tests/tests/kes.py
@@ -12,9 +12,10 @@ class KesScenarios:
     INVALID_KES_PERIOD = "invalid_kes_period"
 
 
-def check_kes_period_info_result(kes_output: dict, expected_scenario: str):
+def check_kes_period_info_result(kes_output: dict, expected_scenario: str, check_id: str) -> str:
     """Check output from kes-period-info command."""
     output_scenario = None
+    kes_period_info_error = ""
 
     # expected scenario: all_valid
     if kes_output.get("valid_counters") and kes_output.get("valid_kes_period"):
@@ -42,8 +43,10 @@ def check_kes_period_info_result(kes_output: dict, expected_scenario: str):
     elif kes_output.get("valid_counters") and not kes_output.get("valid_kes_period"):
         output_scenario = KesScenarios.INVALID_KES_PERIOD
 
-    assert expected_scenario == output_scenario, (
-        "The kes-period-info command is not returning the expected results. "
-        f"Expected: '{expected_scenario}'; Returned: '{output_scenario}'\n"
-        f"Full output:\n{kes_output}"
-    )
+    if expected_scenario != output_scenario:
+        kes_period_info_error = (
+            "The kes-period-info command is not returning the expected results "
+            f"in check '{check_id}'"
+        )
+
+    return kes_period_info_error
