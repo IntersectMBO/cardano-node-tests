38d5003c50ac44d7a3d425bf20832706eccbd68a
Martin Kourim <martin.kourim@iohk.io>
2022-07-14 13:22:09 +0200

    Add `test_minting_two_tokens` using script reference and build-raw

--- cardano_node_tests/tests/test_plutus_v2_mint_raw.py
+++ cardano_node_tests/tests/test_plutus_v2_mint_raw.py
@@ -1,6 +1,8 @@
 """Tests for minting with Plutus V2 using `transaction build-raw`."""
 import logging
+from pathlib import Path
 from typing import List
+from typing import Optional
 from typing import Tuple
 
 import allure
@@ -14,6 +16,7 @@ from cardano_node_tests.utils import cluster_management
 from cardano_node_tests.utils import clusterlib_utils
 from cardano_node_tests.utils import configuration
 from cardano_node_tests.utils import helpers
+from cardano_node_tests.utils import tx_view
 from cardano_node_tests.utils.versions import VERSIONS
 
 LOGGER = logging.getLogger(__name__)
@@ -66,7 +69,13 @@ def _fund_issuer(
     amount: int,
     fee_txsize: int = FEE_MINT_TXSIZE,
     collateral_utxo_num: int = 1,
-) -> Tuple[List[clusterlib.UTXOData], List[clusterlib.UTXOData], clusterlib.TxRawOutput]:
+    reference_script: Optional[Path] = None,
+) -> Tuple[
+    List[clusterlib.UTXOData],
+    List[clusterlib.UTXOData],
+    Optional[clusterlib.UTXOData],
+    clusterlib.TxRawOutput,
+]:
     """Fund the token issuer."""
     single_collateral_amount = minting_cost.collateral // collateral_utxo_num
     collateral_amounts = [single_collateral_amount for c in range(collateral_utxo_num - 1)]
@@ -78,12 +87,31 @@ def _fund_issuer(
     tx_files = clusterlib.TxFiles(
         signing_key_files=[payment_addr.skey_file],
     )
+
+    # for reference script
+    reference_amount = 0
+    txouts_reference = []
+    if reference_script:
+        reference_amount = 10_000_000
+        txouts_reference = [
+            clusterlib.TxOut(
+                address=issuer_addr.address,
+                amount=reference_amount,
+                reference_script_file=reference_script,
+            )
+        ]
+
+    txouts_collateral = [
+        clusterlib.TxOut(address=issuer_addr.address, amount=a) for a in collateral_amounts
+    ]
+
     txouts = [
         clusterlib.TxOut(
             address=issuer_addr.address,
             amount=amount + minting_cost.fee + fee_txsize,
         ),
-        *[clusterlib.TxOut(address=issuer_addr.address, amount=a) for a in collateral_amounts],
+        *txouts_reference,
+        *txouts_collateral,
     ]
 
     tx_raw_output = cluster_obj.send_tx(
@@ -100,17 +128,158 @@ def _fund_issuer(
     issuer_balance = cluster_obj.get_address_balance(issuer_addr.address)
     assert (
         issuer_balance
-        == issuer_init_balance + amount + minting_cost.fee + fee_txsize + minting_cost.collateral
+        == issuer_init_balance
+        + amount
+        + minting_cost.fee
+        + fee_txsize
+        + minting_cost.collateral
+        + reference_amount
     ), f"Incorrect balance for token issuer address `{issuer_addr.address}`"
 
     txid = cluster_obj.get_txid(tx_body_file=tx_raw_output.out_file)
     mint_utxos = cluster_obj.get_utxo(txin=f"{txid}#0")
+
+    reference_utxo = None
+    if reference_script:
+        reference_utxos = cluster_obj.get_utxo(txin=f"{txid}#1")
+        assert reference_utxos, "No reference script UTxO"
+        reference_utxo = reference_utxos[0]
+
     collateral_utxos = [
         clusterlib.UTXOData(utxo_hash=txid, utxo_ix=idx, amount=a, address=issuer_addr.address)
-        for idx, a in enumerate(collateral_amounts, start=1)
+        for idx, a in enumerate(collateral_amounts, start=len(txouts) - len(txouts_collateral))
     ]
 
-    return mint_utxos, collateral_utxos, tx_raw_output
+    return mint_utxos, collateral_utxos, reference_utxo, tx_raw_output
+
+
+@pytest.mark.testnets
+class TestMinting:
+    """Tests for minting using Plutus smart contracts."""
+
+    @allure.link(helpers.get_vcs_link())
+    @pytest.mark.parametrize(
+        "use_reference_script", (True, False), ids=("reference_script", "script_file")
+    )
+    def test_minting_two_tokens(
+        self,
+        cluster: clusterlib.ClusterLib,
+        payment_addrs: List[clusterlib.AddressRecord],
+        use_reference_script: bool,
+        request: FixtureRequest,
+    ):
+        """Test minting two tokens with a single Plutus script.
+
+        * fund the token issuer and create a UTxO for collateral and possibly reference script
+        * check that the expected amount was transferred to token issuer's address
+        * mint the tokens using a Plutus script
+        * check that the tokens were minted and collateral UTxO was not spent
+        """
+        # pylint: disable=too-many-locals
+        temp_template = f"{common.get_test_id(cluster)}_{request.node.callspec.id}"
+
+        payment_addr = payment_addrs[0]
+        issuer_addr = payment_addrs[1]
+
+        lovelace_amount = 2_000_000
+        token_amount = 5
+        fee_txsize = 600_000
+
+        if use_reference_script:
+            execution_cost = plutus_common.MINTING_V2_REF_COST
+        else:
+            execution_cost = plutus_common.MINTING_V2_COST
+
+        minting_cost = plutus_common.compute_cost(
+            execution_cost=execution_cost,
+            protocol_params=cluster.get_protocol_params(),
+        )
+
+        # Step 1: fund the token issuer
+
+        mint_utxos, collateral_utxos, reference_utxo, __ = _fund_issuer(
+            cluster_obj=cluster,
+            temp_template=temp_template,
+            payment_addr=payment_addr,
+            issuer_addr=issuer_addr,
+            minting_cost=minting_cost,
+            amount=lovelace_amount,
+            fee_txsize=fee_txsize,
+            collateral_utxo_num=2,
+            reference_script=plutus_common.MINTING_PLUTUS_V2,
+        )
+        assert reference_utxo or not use_reference_script, "No reference script UTxO"
+
+        issuer_fund_balance = cluster.get_address_balance(issuer_addr.address)
+
+        # Step 2: mint the "qacoin"
+
+        policyid = cluster.get_policyid(plutus_common.MINTING_PLUTUS_V2)
+        asset_name_a = f"qacoina{clusterlib.get_rand_str(4)}".encode("utf-8").hex()
+        token_a = f"{policyid}.{asset_name_a}"
+        asset_name_b = f"qacoinb{clusterlib.get_rand_str(4)}".encode("utf-8").hex()
+        token_b = f"{policyid}.{asset_name_b}"
+        mint_txouts = [
+            clusterlib.TxOut(address=issuer_addr.address, amount=token_amount, coin=token_a),
+            clusterlib.TxOut(address=issuer_addr.address, amount=token_amount, coin=token_b),
+        ]
+
+        plutus_mint_data = [
+            clusterlib.Mint(
+                txouts=mint_txouts,
+                script_file=plutus_common.MINTING_PLUTUS_V2 if not use_reference_script else "",
+                reference_txin=reference_utxo if use_reference_script else None,
+                collaterals=collateral_utxos,
+                execution_units=(
+                    execution_cost.per_time,
+                    execution_cost.per_space,
+                ),
+                redeemer_cbor_file=plutus_common.REDEEMER_42_CBOR,
+                policyid=policyid,
+            )
+        ]
+
+        tx_files_step2 = clusterlib.TxFiles(
+            signing_key_files=[issuer_addr.skey_file],
+        )
+        txouts_step2 = [
+            clusterlib.TxOut(address=issuer_addr.address, amount=lovelace_amount),
+            *mint_txouts,
+        ]
+        tx_raw_output_step2 = cluster.build_raw_tx_bare(
+            out_file=f"{temp_template}_step2_tx.body",
+            txins=mint_utxos,
+            txouts=txouts_step2,
+            mint=plutus_mint_data,
+            tx_files=tx_files_step2,
+            fee=minting_cost.fee + fee_txsize,
+            # ttl is optional in this test
+            invalid_hereafter=cluster.get_slot_no() + 200,
+        )
+        tx_signed_step2 = cluster.sign_tx(
+            tx_body_file=tx_raw_output_step2.out_file,
+            signing_key_files=tx_files_step2.signing_key_files,
+            tx_name=f"{temp_template}_step2",
+        )
+        cluster.submit_tx(tx_file=tx_signed_step2, txins=mint_utxos)
+
+        assert (
+            cluster.get_address_balance(issuer_addr.address)
+            == issuer_fund_balance - tx_raw_output_step2.fee
+        ), f"Incorrect balance for token issuer address `{issuer_addr.address}`"
+
+        token_utxo_a = cluster.get_utxo(address=issuer_addr.address, coins=[token_a])
+        assert (
+            token_utxo_a and token_utxo_a[0].amount == token_amount
+        ), "The 'token a' was not minted"
+
+        token_utxo_b = cluster.get_utxo(address=issuer_addr.address, coins=[token_b])
+        assert (
+            token_utxo_b and token_utxo_b[0].amount == token_amount
+        ), "The 'token b' was not minted"
+
+        # check tx view
+        tx_view.check_tx_view(cluster_obj=cluster, tx_raw_output=tx_raw_output_step2)
 
 
 @pytest.mark.testnets
