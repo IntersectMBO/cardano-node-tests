1a5004b23d4bd01fd84e602cdbde1c6f67cee04a
Martin Kourim <martin.kourim@iohk.io>
2022-08-15 22:48:16 +0200

    Update clusterlib dependency to 0.3.0rc16

--- nix/cardano-clusterlib.nix
+++ nix/cardano-clusterlib.nix
@@ -2,10 +2,10 @@
 
 buildPythonPackage rec {
   pname = "cardano-clusterlib";
-  version = "0.3.0rc15";
+  version = "0.3.0rc16";
   src = fetchPypi {
     inherit pname version;
-    sha256 = "2+EhXGnNn7HiYPiTxj5AwtYZVBV8iti72Wp9T5FM2+c=";
+    sha256 = "z0ka720Rm8HheWeOCGZAqYR2c+CBQMLlrGj9TKEevrw=";
   };
   doCheck = false;
   nativeBuildInputs = [ setuptools_scm ];
