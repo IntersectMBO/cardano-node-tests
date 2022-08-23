226cff8206a18f376798f4e012013cfd72307e25
Martin Kourim <martin.kourim@iohk.io>
2022-08-23 15:40:51 +0200

    Tests for submitting old Tx
    The Tx was created by older node version and/or in previous era.

--- cardano_node_tests/tests/test_node_upgrade.py
+++ cardano_node_tests/tests/test_node_upgrade.py
@@ -2,6 +2,7 @@
 import json
 import logging
 import os
+import shutil
 from pathlib import Path
 from typing import List
 
@@ -17,6 +18,7 @@ from cardano_node_tests.utils import clusterlib_utils
 from cardano_node_tests.utils import configuration
 from cardano_node_tests.utils import helpers
 from cardano_node_tests.utils import logfiles
+from cardano_node_tests.utils import temptools
 
 LOGGER = logging.getLogger(__name__)
 
@@ -70,6 +72,30 @@ def payment_addrs(
     return addrs
 
 
+@pytest.fixture
+def payment_addrs_disposable(
+    cluster_manager: cluster_management.ClusterManager,
+    cluster: clusterlib.ClusterLib,
+) -> List[clusterlib.AddressRecord]:
+    """Create new disposable payment addresses."""
+    temp_template = common.get_test_id(cluster)
+
+    addrs = clusterlib_utils.create_payment_addr_records(
+        f"{temp_template}_payment_addr_disposable_0",
+        f"{temp_template}_payment_addr_disposable_1",
+        cluster_obj=cluster,
+    )
+
+    # fund source addresses
+    clusterlib_utils.fund_from_faucet(
+        addrs[0],
+        cluster_obj=cluster,
+        faucet_data=cluster_manager.cache.addrs_data["user1"],
+    )
+
+    return addrs
+
+
 class TestUpgrade:
     @allure.link(helpers.get_vcs_link())
     @pytest.mark.skipif(UPGRADE_TESTS_STEP != 2, reason="runs only on step 2 of upgrade testing")
@@ -91,6 +117,181 @@ class TestUpgrade:
                 regex="ChainDB:Error:.* Invalid snapshot DiskSnapshot .*DeserialiseFailure 168",
                 ignore_file_id=worker_id,
             )
+
+    @allure.link(helpers.get_vcs_link())
+    @pytest.mark.skipif(UPGRADE_TESTS_STEP > 2, reason="doesn't run on step > 2 of upgrade testing")
+    @pytest.mark.order(-1)
+    @pytest.mark.smoke
+    @common.PARAM_USE_BUILD_CMD
+    @pytest.mark.parametrize(
+        "for_step",
+        (
+            pytest.param(
+                2,
+                marks=pytest.mark.skipif(
+                    UPGRADE_TESTS_STEP == 2, reason="doesn't run on step 2 of upgrade testing"
+                ),
+            ),
+            pytest.param(
+                3,
+                marks=pytest.mark.skipif(
+                    UPGRADE_TESTS_STEP == 3, reason="doesn't run on step 3 of upgrade testing"
+                ),
+            ),
+        ),
+    )
+    @pytest.mark.parametrize("file_type", ("tx", "tx_body"))
+    def test_prepare_tx(
+        self,
+        cluster_manager: cluster_management.ClusterManager,
+        cluster: clusterlib.ClusterLib,
+        payment_addrs_disposable: List[clusterlib.AddressRecord],
+        use_build_cmd: bool,
+        for_step: int,
+        file_type: str,
+    ):
+        """Prepare transactions that will be submitted in next steps of upgrade testing.
+
+        For testing that transaction created by previous node version and/or in previous era can
+        be submitted in next node version and/or next era.
+        """
+        temp_template = f"{common.get_test_id(cluster)}_{use_build_cmd}_{for_step}_{file_type}"
+        build_str = "build" if use_build_cmd else "build_raw"
+
+        src_address = payment_addrs_disposable[0].address
+        dst_address = payment_addrs_disposable[1].address
+
+        destinations = [clusterlib.TxOut(address=dst_address, amount=2_000_000)]
+        tx_files = clusterlib.TxFiles(signing_key_files=[payment_addrs_disposable[0].skey_file])
+
+        if use_build_cmd:
+            tx_raw_output = cluster.build_tx(
+                src_address=src_address,
+                tx_name=temp_template,
+                tx_files=tx_files,
+                txouts=destinations,
+                fee_buffer=1_000_000,
+            )
+            out_file_signed = cluster.sign_tx(
+                tx_body_file=tx_raw_output.out_file,
+                signing_key_files=tx_files.signing_key_files,
+                tx_name=temp_template,
+            )
+        else:
+            fee = cluster.calculate_tx_fee(
+                src_address=src_address,
+                tx_name=temp_template,
+                txouts=destinations,
+                tx_files=tx_files,
+            )
+            tx_raw_output = cluster.build_raw_tx(
+                src_address=src_address,
+                tx_name=temp_template,
+                txouts=destinations,
+                tx_files=tx_files,
+                fee=fee,
+            )
+            out_file_signed = cluster.sign_tx(
+                tx_body_file=tx_raw_output.out_file,
+                signing_key_files=tx_files.signing_key_files,
+                tx_name=temp_template,
+            )
+
+        copy_files = [
+            payment_addrs_disposable[0].skey_file,
+            tx_raw_output.out_file,
+            out_file_signed,
+        ]
+
+        tx_dir = (
+            temptools.get_basetemp()
+            / cluster_manager.cache.last_checksum
+            / f"{UPGRADE_TESTS_STEP}for{for_step}"
+            / file_type
+            / build_str
+        ).resolve()
+
+        if tx_dir.exists():
+            shutil.rmtree(tx_dir)
+        tx_dir.mkdir(parents=True)
+
+        for f in copy_files:
+            shutil.copy(f, tx_dir)
+
+    @allure.link(helpers.get_vcs_link())
+    @pytest.mark.skipif(UPGRADE_TESTS_STEP < 2, reason="runs only on step >= 2 of upgrade testing")
+    @pytest.mark.order(5)
+    @pytest.mark.smoke
+    @common.PARAM_USE_BUILD_CMD
+    @pytest.mark.parametrize(
+        "for_step",
+        (
+            pytest.param(
+                2,
+                marks=pytest.mark.skipif(
+                    UPGRADE_TESTS_STEP != 2, reason="runs only on step 2 of upgrade testing"
+                ),
+            ),
+            pytest.param(
+                3,
+                marks=pytest.mark.skipif(
+                    UPGRADE_TESTS_STEP != 3, reason="runs only on step 3 of upgrade testing"
+                ),
+            ),
+        ),
+    )
+    @pytest.mark.parametrize(
+        "from_step",
+        (
+            1,
+            pytest.param(
+                2,
+                marks=pytest.mark.skipif(
+                    UPGRADE_TESTS_STEP == 2, reason="doesn't run on step 2 of upgrade testing"
+                ),
+            ),
+        ),
+    )
+    @pytest.mark.parametrize("file_type", ("tx", "tx_body"))
+    def test_submit_tx(
+        self,
+        cluster_manager: cluster_management.ClusterManager,
+        cluster: clusterlib.ClusterLib,
+        use_build_cmd: bool,
+        for_step: int,
+        from_step: int,
+        file_type: str,
+    ):
+        """Submit transaction that was created by previous node version and/or in previous era."""
+        temp_template = (
+            f"{common.get_test_id(cluster)}_{use_build_cmd}_{from_step}_{for_step}_{file_type}"
+        )
+        build_str = "build" if use_build_cmd else "build_raw"
+
+        tx_dir = (
+            temptools.get_basetemp()
+            / cluster_manager.cache.last_checksum
+            / f"{from_step}for{for_step}"
+            / file_type
+            / build_str
+        ).resolve()
+
+        if not tx_dir.exists():
+            pytest.skip("No tx files found")
+
+        tx_file = list(tx_dir.glob("*.signed"))[0]
+        tx_body_file = list(tx_dir.glob("*.body"))[0]
+        skey_file = list(tx_dir.glob("*.skey"))[0]
+
+        if file_type == "tx_body":
+            tx_file = cluster.sign_tx(
+                tx_body_file=tx_body_file,
+                tx_name=temp_template,
+                signing_key_files=[skey_file],
+            )
+
+        cluster.submit_tx_bare(tx_file=tx_file)
+        cluster.wait_for_new_block(2)
 
     @allure.link(helpers.get_vcs_link())
     @pytest.mark.skipif(UPGRADE_TESTS_STEP != 3, reason="runs only on step 3 of upgrade testing")
