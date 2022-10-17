0d24f4b3a43ed9c66bc203e9568b6b11eabfd757
saratomaz <sara.tomaz@iohk.io>
2022-10-17 10:25:21 +0100

    check if plutus scripts see reference scripts

--- cardano_node_tests/tests/plutus_common.py
+++ cardano_node_tests/tests/plutus_common.py
@@ -40,6 +40,7 @@ MINTING_TOKENNAME_PLUTUS_V1 = SCRIPTS_V1_DIR / "mint-tokenname.plutus"
 MINTING_PLUTUS_V2 = SCRIPTS_V2_DIR / "anyone-can-mint.plutus"
 MINTING_CHECK_REF_INPUTS_PLUTUS_V2 = SCRIPTS_V2_DIR / "check-mint-with-reference-inputs.plutus"
 MINTING_CHECK_DATUM_HASH_PLUTUS_V2 = SCRIPTS_V2_DIR / "check-mint-datum-hash.plutus"
+MINTING_CHECK_REF_SCRIPTS_PLUTUS_V2 = SCRIPTS_V2_DIR / "check-mint-with-reference-scripts.plutus"
 
 STAKE_GUESS_42_PLUTUS_V1 = SCRIPTS_V1_DIR / "guess-42-stake.plutus"
 
@@ -107,6 +108,9 @@ MINTING_V2_CHECK_REF_INPUTS_COST = ExecutionCost(
 )
 MINTING_V2_CHECK_DATUM_HASH_COST = ExecutionCost(
     per_time=244_944_118, per_space=797_302, fixed_cost=63_665
+)
+MINTING_V2_CHECK_REF_SCRIPTS_COST = ExecutionCost(
+    per_time=208_713_230, per_space=678_512, fixed_cost=54_199
 )
 
 # TODO: cost in old Alonzo cost model
