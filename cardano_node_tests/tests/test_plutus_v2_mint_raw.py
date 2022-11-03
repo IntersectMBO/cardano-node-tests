0358461b39af3eeea95581ed52ed863834dc08fe
Sudip Bhattarai <071bct544@pcampus.edu.np>
2022-11-03 21:26:41 +0545

    Update redeemers for secp256k1 test and check error messages for negative tests (#1469)
    * Update redeemers for secp256k1 test and check error messages for negative tests
    
    * Fix lint errors
    
    * fix regex match for mint_raw secp fucntion test
    
    * update secp test to work with PV7 and PV8
    
    * update secp positive scenario with PV7 expected failure
    
    * Better utxo filtering; more detailed assert messages
    
    Co-authored-by: Sudip Bhattarai <mesudip@github.com>
    Co-authored-by: saratomaz <sara.tomaz@iohk.io>
    Co-authored-by: Martin Kourim <martin.kourim@iohk.io>

--- cardano_node_tests/tests/test_plutus_v2_mint_raw.py
+++ cardano_node_tests/tests/test_plutus_v2_mint_raw.py
@@ -695,28 +695,16 @@ class TestNegativeCollateralOutput:
 
 @pytest.mark.testnets
 class TestSECP256k1:
-    @allure.link(helpers.get_vcs_link())
-    @pytest.mark.parametrize(
-        "test_vector",
-        ("positive", "invalid_sig", "invalid_pubkey", "no_msg", "no_pubkey", "no_sig"),
-    )
-    @pytest.mark.parametrize("algorithm", ("ecdsa", "schnorr"))
-    def test_use_secp_builtin_functions(
+    def _fund_issuer_mint_token(
         self,
-        cluster: clusterlib.ClusterLib,
+        cluster_obj: clusterlib.ClusterLib,
+        temp_template: str,
         payment_addrs: List[clusterlib.AddressRecord],
-        test_vector: str,
-        algorithm: str,
+        script_file: Path,
+        redeemer_file: Path,
     ):
-        """Test that the two SECP256k1 builtin functions are impossible to use.
-
-        * lock some funds with the dedicated plutus script
-        * try to spend the locked UTxO
-        * check that is not possible to use SECP256k1
-        """
-        # pylint: disable=too-many-locals
+        """Fund the token issuer and mint a token."""
         __: Any  # mypy workaround
-        temp_template = f"{common.get_test_id(cluster)}_{test_vector}_{algorithm}"
         payment_addr = payment_addrs[0]
         issuer_addr = payment_addrs[1]
 
@@ -726,17 +714,11 @@ class TestSECP256k1:
 
         minting_cost = plutus_common.compute_cost(
             execution_cost=plutus_common.MINTING_V2_REF_COST,
-            protocol_params=cluster.g_query.get_protocol_params(),
-        )
-
-        script_file = (
-            plutus_common.SECP256K1_ECDSA_PLUTUS_V2
-            if algorithm == "ecdsa"
-            else plutus_common.SECP256K1_SCHNORR_PLUTUS_V2
+            protocol_params=cluster_obj.g_query.get_protocol_params(),
         )
 
         mint_utxos, collateral_utxos, __, __ = _fund_issuer(
-            cluster_obj=cluster,
+            cluster_obj=cluster_obj,
             temp_template=temp_template,
             payment_addr=payment_addr,
             issuer_addr=issuer_addr,
@@ -745,17 +727,7 @@ class TestSECP256k1:
             fee_txsize=fee_txsize,
         )
 
-        redeemer_dir = (
-            plutus_common.SEPC256K1_ECDSA_DIR
-            if algorithm == "ecdsa"
-            else plutus_common.SEPC256K1_SCHNORR_DIR
-        )
-
-        redeemer_file = redeemer_dir / f"{test_vector}.redeemer"
-
-        # Step 2: mint the "qacoin"
-
-        policyid = cluster.g_transaction.get_policyid(script_file)
+        policyid = cluster_obj.g_transaction.get_policyid(script_file)
         asset_name = f"qacoin{clusterlib.get_rand_str(4)}".encode("utf-8").hex()
         token = f"{policyid}.{asset_name}"
         mint_txouts = [
@@ -784,7 +756,7 @@ class TestSECP256k1:
             *mint_txouts,
         ]
 
-        tx_raw_output_step2 = cluster.g_transaction.build_raw_tx_bare(
+        tx_raw_output_step2 = cluster_obj.g_transaction.build_raw_tx_bare(
             out_file=f"{temp_template}_step2_tx.body",
             txins=mint_utxos,
             txouts=txouts_step2,
@@ -793,21 +765,68 @@ class TestSECP256k1:
             fee=minting_cost.fee + fee_txsize,
         )
 
-        tx_signed_step2 = cluster.g_transaction.sign_tx(
+        tx_signed_step2 = cluster_obj.g_transaction.sign_tx(
             tx_body_file=tx_raw_output_step2.out_file,
             signing_key_files=tx_files_step2.signing_key_files,
             tx_name=f"{temp_template}_step2",
         )
 
-        protocol_version = cluster.g_query.get_protocol_params()["protocolVersion"]["major"]
+        cluster_obj.g_transaction.submit_tx(tx_file=tx_signed_step2, txins=mint_utxos)
+
+        out_utxos = cluster_obj.g_query.get_utxo(tx_raw_output=tx_raw_output_step2)
+        token_utxo = clusterlib.filter_utxos(
+            utxos=out_utxos, address=issuer_addr.address, coin=token
+        )
+        assert token_utxo and token_utxo[0].amount == token_amount, "The token was not minted"
+
+    @allure.link(helpers.get_vcs_link())
+    @pytest.mark.parametrize("algorithm", ("ecdsa", "schnorr"))
+    def test_use_secp_builtin_functions(
+        self,
+        cluster: clusterlib.ClusterLib,
+        payment_addrs: List[clusterlib.AddressRecord],
+        algorithm: str,
+    ):
+        """Test that is possible to use the two SECP256k1 builtin functions.
+
+        * fund the token issuer
+        * mint the tokens using a Plutus script with a SECP256k1 function
+        * check that the token was minted
+        """
+        temp_template = f"{common.get_test_id(cluster)}_positive_{algorithm}"
+
+        script_file = (
+            plutus_common.SECP256K1_ECDSA_PLUTUS_V2
+            if algorithm == "ecdsa"
+            else plutus_common.SECP256K1_SCHNORR_PLUTUS_V2
+        )
+
+        redeemer_dir = (
+            plutus_common.SEPC256K1_ECDSA_DIR
+            if algorithm == "ecdsa"
+            else plutus_common.SEPC256K1_SCHNORR_DIR
+        )
+
+        redeemer_file = redeemer_dir / "positive.redeemer"
 
         try:
-            cluster.g_transaction.submit_tx(tx_file=tx_signed_step2, txins=mint_utxos)
+            self._fund_issuer_mint_token(
+                cluster_obj=cluster,
+                temp_template=temp_template,
+                payment_addrs=payment_addrs,
+                script_file=script_file,
+                redeemer_file=redeemer_file,
+            )
         except clusterlib.CLIError as err:
-            err_msg = str(err)
+            before_pv8 = cluster.g_query.get_protocol_params()["protocolVersion"]["major"] < 8
 
-            # before protocol_version 8 the SECP256k1 is blocked
-            # after that the usage is limited by high cost model
+            # the SECP256k1 functions should work from protocol version 8
+            if not before_pv8:
+                raise
+
+            # before protocol version 8 the SECP256k1 is blocked
+            # or limited by high cost model
+            err_msg = str(err)
 
             is_forbidden = "MalformedScriptWitnesses" in err_msg
 
@@ -816,8 +835,84 @@ class TestSECP256k1:
                 "overspending the budget." in err_msg
             )
 
-            if (is_forbidden or is_overspending) and protocol_version < 8:
+            if is_forbidden or is_overspending:
                 pytest.xfail(
                     "The SECP256k1 builtin functions are not allowed before protocol version 8"
                 )
             raise
+
+    @allure.link(helpers.get_vcs_link())
+    @pytest.mark.parametrize(
+        "test_vector",
+        ("invalid_sig", "invalid_pubkey", "no_msg", "no_pubkey", "no_sig"),
+    )
+    @pytest.mark.parametrize("algorithm", ("ecdsa", "schnorr"))
+    def test_negative_secp_builtin_functions(
+        self,
+        cluster: clusterlib.ClusterLib,
+        payment_addrs: List[clusterlib.AddressRecord],
+        test_vector: str,
+        algorithm: str,
+    ):
+        """Try to mint a token with invalid test vectors.
+
+        * Expect failure.
+        """
+        temp_template = f"{common.get_test_id(cluster)}_{test_vector}_{algorithm}"
+
+        script_file = (
+            plutus_common.SECP256K1_ECDSA_PLUTUS_V2
+            if algorithm == "ecdsa"
+            else plutus_common.SECP256K1_SCHNORR_PLUTUS_V2
+        )
+
+        redeemer_dir = (
+            plutus_common.SEPC256K1_ECDSA_DIR
+            if algorithm == "ecdsa"
+            else plutus_common.SEPC256K1_SCHNORR_DIR
+        )
+
+        redeemer_file = redeemer_dir / f"{test_vector}.redeemer"
+
+        before_pv8 = cluster.g_query.get_protocol_params()["protocolVersion"]["major"] < 8
+
+        with pytest.raises(clusterlib.CLIError) as excinfo:
+            self._fund_issuer_mint_token(
+                cluster_obj=cluster,
+                temp_template=temp_template,
+                payment_addrs=payment_addrs,
+                script_file=script_file,
+                redeemer_file=redeemer_file,
+            )
+
+        err_msg = str(excinfo.value)
+
+        # before  rotocol_version 8 the SECP256k1 is blocked
+        # after that the usage is limited by high cost model
+        is_forbidden = "MalformedScriptWitnesses" in err_msg
+
+        is_overspending = (
+            "The machine terminated part way through evaluation due to "
+            "overspending the budget." in err_msg
+        )
+
+        # from protocol version 8 the SECP256k1 functions are allowed
+        decoding_error = f"Caused by: (verify{algorithm.capitalize()}Secp256k1Signature"
+
+        validation_error = (
+            "The machine terminated because of an error, "
+            "either from a built-in function or from an explicit use of 'error'."
+        )
+
+        expected_error_messages = {
+            "invalid_sig": validation_error,
+            "invalid_pubkey": validation_error,
+            "no_msg": decoding_error if algorithm == "ecdsa" else validation_error,
+            "no_pubkey": decoding_error,
+            "no_sig": decoding_error,
+        }
+
+        if before_pv8:
+            assert is_forbidden or is_overspending, err_msg
+        else:
+            assert expected_error_messages[test_vector] in err_msg, err_msg
