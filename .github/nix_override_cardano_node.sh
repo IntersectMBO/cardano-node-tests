409f64c5086c437496deb78904f8ec27276ced65
Martin Kourim <martin.kourim@iohk.io>
2023-01-27 15:16:49 +0100

    Better delete; stop running cluster instance

--- .github/nix_override_cardano_node.sh
+++ .github/nix_override_cardano_node.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
 node_override() {
+  local ref
   # if argument is provided:
   if [ -n "${1:-""}" ]; then
-    REF="/$1"
+    ref="/$1"
   # else use specified branch and/or revision:
   elif [ -n "${NODE_REV:-""}" ]; then
-    REF="/$NODE_REV"
+    ref="/$NODE_REV"
   else
     # otherwise update to latest from default branch
-    REF=""
+    ref=""
   fi
-  echo --override-input cardano-node "github:input-output-hk/cardano-node$REF" --recreate-lock-file
+  echo --override-input cardano-node "github:input-output-hk/cardano-node$ref" --recreate-lock-file
 }
