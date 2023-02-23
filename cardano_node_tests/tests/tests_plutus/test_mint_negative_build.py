243e1ab622d8bd9d1e74b491d46da4d6a4564ae3
Martin Kourim <martin.kourim@iohk.io>
2023-02-23 16:14:08 +0100

    Add `build` version of `test_time_range_missing_tx_validity`

--- cardano_node_tests/tests/tests_plutus/test_mint_negative_build.py
+++ cardano_node_tests/tests/tests_plutus/test_mint_negative_build.py
@@ -1,4 +1,5 @@
 """Negative tests for minting with Plutus using `transaction build`."""
+import datetime
 import json
 import logging
 from pathlib import Path
@@ -340,3 +341,89 @@ class TestBuildMintingNegative:
             "the bytestring should be no longer than 32 bytes long" in min_req_utxo_error
             or "AssetName deserisalisation failed" in min_req_utxo_error
         ), min_req_utxo_error
+
+    @allure.link(helpers.get_vcs_link())
+    @pytest.mark.testnets
+    def test_time_range_missing_tx_validity(
+        self, cluster: clusterlib.ClusterLib, payment_addrs: List[clusterlib.AddressRecord]
+    ):
+        """Test minting a token with a time constraints Plutus script and no TX validity.
+
+        Expect failure.
+
+        Uses `cardano-cli transaction build` command for building the transactions.
+
+        * fund the token issuer and create a UTxO for collateral
+        * check that the expected amount was transferred to token issuer's address
+        * try to mint the token using a Plutus script and a TX without validity interval
+        * check that the minting failed
+        """
+        # pylint: disable=too-many-locals
+        temp_template = common.get_test_id(cluster)
+        payment_addr = payment_addrs[0]
+        issuer_addr = payment_addrs[1]
+
+        lovelace_amount = 2_000_000
+        token_amount = 5
+        script_fund = 200_000_000
+
+        minting_cost = plutus_common.compute_cost(
+            execution_cost=plutus_common.MINTING_TIME_RANGE_COST,
+            protocol_params=cluster.g_query.get_protocol_params(),
+        )
+
+        # Step 1: fund the token issuer
+
+        mint_utxos, collateral_utxos, __ = mint_build._fund_issuer(
+            cluster_obj=cluster,
+            temp_template=temp_template,
+            payment_addr=payment_addr,
+            issuer_addr=issuer_addr,
+            minting_cost=minting_cost,
+            amount=script_fund,
+        )
+
+        # Step 2: mint the "qacoin"
+
+        slots_offset = 300
+        timestamp_offset_ms = int(slots_offset * cluster.slot_length + 5) * 1_000
+
+        # POSIX timestamp + offset
+        redeemer_value = int(datetime.datetime.now().timestamp() * 1_000) + timestamp_offset_ms
+
+        policyid = cluster.g_transaction.get_policyid(plutus_common.MINTING_TIME_RANGE_PLUTUS_V1)
+        asset_name = f"qacoin{clusterlib.get_rand_str(4)}".encode().hex()
+        token = f"{policyid}.{asset_name}"
+        mint_txouts = [
+            clusterlib.TxOut(address=issuer_addr.address, amount=token_amount, coin=token)
+        ]
+
+        plutus_mint_data = [
+            clusterlib.Mint(
+                txouts=mint_txouts,
+                script_file=plutus_common.MINTING_TIME_RANGE_PLUTUS_V1,
+                collaterals=collateral_utxos,
+                redeemer_value=str(redeemer_value),
+            )
+        ]
+
+        tx_files_step2 = clusterlib.TxFiles(
+            signing_key_files=[issuer_addr.skey_file],
+        )
+        txouts_step2 = [
+            clusterlib.TxOut(address=issuer_addr.address, amount=lovelace_amount),
+            *mint_txouts,
+        ]
+
+        with pytest.raises(clusterlib.CLIError) as excinfo:
+            cluster.g_transaction.build_tx(
+                src_address=payment_addr.address,
+                tx_name=f"{temp_template}_step2",
+                tx_files=tx_files_step2,
+                txins=mint_utxos,
+                txouts=txouts_step2,
+                mint=plutus_mint_data,
+                invalid_hereafter=None,  # required validity interval is missing here
+            )
+        err_str = str(excinfo.value)
+        assert "Plutus script evaluation failed" in err_str, err_str
