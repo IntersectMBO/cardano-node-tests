5ccc9dfc5b01ede3ce10b02d6552f7f09570b6e9
Martin Kourim <martin.kourim@iohk.io>
2023-05-18 23:09:54 +0200

    Add support for checking `fixed_in` for dbsync

--- cardano_node_tests/utils/blockers.py
+++ cardano_node_tests/utils/blockers.py
@@ -16,24 +16,49 @@ if os.environ.get("GH_TOKEN"):
 
 
 class GH:
-    """GitHub issues blocker."""
+    """Methods for working with cardano cluster using `cardano-cli`..
+
+    Attributes:
+        issue: A GitHub issue number.
+        repo: A repository where the issue belongs to. Default: `input-output-hk/cardano-node`.
+        fixed_in: A version of the project where the issue is fixed. Ignored on unknown projects.
+        message: A message to be added to blocking outcome.
+        check_on_devel: A boolean flag indicating if the issue should be checked on devel versions
+            of the project. Default: `True`.
+    """
 
     def __init__(
         self,
         issue: int,
         repo: str = "input-output-hk/cardano-node",
         fixed_in: str = "",
+        message: str = "",
         check_on_devel: bool = True,
     ) -> None:
         self.issue = issue
         self.repo = repo
         self.fixed_in = fixed_in
+        self.message = message
         self.check_on_devel = check_on_devel
         self.repo_name = repo.split("/")[-1]
         self.gh_issue = gh_issue.GHIssue(number=self.issue, repo=self.repo)
 
+        self._project = None
+        if self.repo == "input-output-hk/cardano-node":
+            self._project = "node"
+        elif self.repo == "input-output-hk/cardano-db-sync":
+            self._project = "dbsync"
+
     def is_blocked(self) -> bool:
         """Check if issue is blocked."""
+        if self._project == "node":
+            return self._node_issue_is_blocked()
+        if self._project == "dbsync":
+            return self._dbsync_issue_is_blocked()
+        return self._issue_is_blocked()
+
+    def _node_issue_is_blocked(self) -> bool:
+        """Check if node issue is blocked."""
         # Assume that the issue is blocked if we are not supposed to check the issue on
         # devel versions of node and we are running a devel version.
         # This can be useful when the issue was fixed in a released version of node, but
@@ -68,14 +93,65 @@ class GH:
 
         return False
 
-    def finish_test(self, message: str) -> None:
-        """Fail or Xfail test with GitHub issue reference."""
-        reason = f"{self.gh_issue}: {message}"
+    def _dbsync_issue_is_blocked(self) -> bool:
+        """Check if dbsync issue is blocked."""
+        # Assume that the issue is blocked if we are not supposed to check the issue on
+        # devel versions of dbsync and we are running a devel version.
+        # This can be useful when the issue was fixed in a released version of dbsync, but
+        # the fix was not merged into the master branch yet.
+        if VERSIONS.dbsync_is_devel and not self.check_on_devel:
+            return True
 
-        # Xfail if the issue is still blocked
+        # Assume that the issue is blocked if no Github token was provided and so the check
+        # cannot be performed.
+        if not self.gh_issue.TOKEN:
+            LOGGER.warning(
+                "No GitHub token provided, cannot check if issue '%s' is blocked",
+                f"{self.repo}#{self.issue}",
+            )
+            return True
+
+        # The issue is blocked if it is was not closed yet
+        if not self.gh_issue.is_closed():
+            return True
+
+        # If here, the issue is already closed
+
+        # We assume all fixes are merged into the master branch. Therefore if we are running
+        # devel version of dbsync, the issue is not blocked if it was closed.
+        if VERSIONS.dbsync_is_devel:
+            return False
+
+        # The issue is blocked if it was fixed in a dbsync version that is greater than
+        # the dbsync version we are currently running.
+        if self.fixed_in and version.parse(self.fixed_in) > VERSIONS.dbsync:
+            return True
+
+        return False
+
+    def _issue_is_blocked(self) -> bool:
+        """Check if generic issue is blocked."""
+        # Assume that the issue is blocked if no Github token was provided and so the check
+        # cannot be performed.
+        if not self.gh_issue.TOKEN:
+            LOGGER.warning(
+                "No GitHub token provided, cannot check if issue '%s' is blocked",
+                f"{self.repo}#{self.issue}",
+            )
+            return True
+
+        # The issue is blocked if it is was not closed yet
+        if not self.gh_issue.is_closed():
+            return True
+
+        return False
+
+    def finish_test(self) -> None:
+        """Fail or Xfail test with GitHub issue reference."""
+        reason = f"{self.gh_issue}: {self.message}"
+
         if self.is_blocked():
             pytest.xfail(reason)
-        # Fail if the issue is supposed to be fixed
         else:
             pytest.fail(reason)
 
