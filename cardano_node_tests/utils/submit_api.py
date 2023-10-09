589c9598e074dc6c1238b6c9271b9dc80fd00147
Martin Kourim <martin.kourim@iohk.io>
2023-10-09 17:54:12 +0200

    Check that a Tx was added to chain when using submit-api

--- cardano_node_tests/utils/submit_api.py
+++ cardano_node_tests/utils/submit_api.py
@@ -1,14 +1,18 @@
 """Utilities for `cardano-submit-api` REST service."""
 import binascii
 import json
+import logging
 import pathlib as pl
 import shutil
 import typing as tp
 
 import requests
+from cardano_clusterlib import clusterlib
 
 import cardano_node_tests.utils.types as ttypes
 from cardano_node_tests.utils import cluster_nodes
+
+LOGGER = logging.getLogger(__name__)
 
 
 class SubmitApiError(Exception):
@@ -57,7 +61,7 @@ def post_cbor(cbor_file: ttypes.FileType, url: str) -> requests.Response:
     return response
 
 
-def submit_tx(tx_file: ttypes.FileType) -> SubmitApiOut:
+def submit_tx_bare(tx_file: ttypes.FileType) -> SubmitApiOut:
     """Submit a signed Tx using `cardano-submit-api` service."""
     cbor_file = tx2cbor(tx_file=tx_file)
 
@@ -81,3 +85,58 @@ def submit_tx(tx_file: ttypes.FileType) -> SubmitApiOut:
     out = SubmitApiOut(txid=response.json(), response=response)
 
     return out
+
+
+def submit_tx(
+    cluster_obj: clusterlib.ClusterLib,
+    tx_file: ttypes.FileType,
+    txins: tp.List[clusterlib.UTXOData],
+    wait_blocks: int = 2,
+) -> None:
+    """Submit a transaction, resubmit if the transaction didn't make it to the chain.
+
+    Args:
+        cluster_obj: An instance of `clusterlib.ClusterLib`.
+        tx_file: A path to signed transaction file.
+        txins: An iterable of `clusterlib.UTXOData`, specifying input UTxOs.
+        wait_blocks: A number of new blocks to wait for (default = 2).
+    """
+    txid = ""
+    for r in range(20):
+        err = None
+
+        if r == 0:
+            txid = submit_tx_bare(tx_file).txid
+        else:
+            assert txid
+            LOGGER.warning(f"Resubmitting transaction '{txid}' (from '{tx_file}').")
+            try:
+                submit_tx_bare(tx_file)
+            except SubmitApiError as exc:
+                # Check if resubmitting failed because an input UTxO was already spent
+                if "(BadInputsUTxO" not in str(exc):
+                    raise
+                err = exc
+                # If here, the TX is likely still in mempool and we need to wait
+
+        cluster_obj.wait_for_new_block(wait_blocks)
+
+        # Check that one of the input UTxOs can no longer be queried in order to verify
+        # the TX was successfully submitted to the chain (that the TX is no longer in mempool).
+        # An input is spent when its combination of hash and ix is not found in the list
+        # of current UTxOs.
+        # TODO: check that the transaction is 1-block deep (can't be done in CLI alone)
+        utxo_data = cluster_obj.g_query.get_utxo(utxo=txins[0])
+        if not utxo_data:
+            break
+    else:
+        if err is not None:
+            # Submitting the TX raised an exception as if the input was already
+            # spent, but it was either not the case, or the TX is still in mempool.
+            raise SubmitApiError(
+                f"Failed to resubmit the transaction '{txid}' (from '{tx_file}')."
+            ) from err
+
+        raise SubmitApiError(
+            f"Transaction '{txid}' didn't make it to the chain (from '{tx_file}')."
+        )
