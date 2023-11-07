51e5a1354f35c42c510696c04c30d1a69cc18e1c
Martin Kourim <martin.kourim@iohk.io>
2023-11-07 14:27:41 +0100

    Remove unneeded nix input override

--- .github/nix_override_cardano_node.sh
+++ .github/nix_override_cardano_node.sh
@@ -13,8 +13,5 @@ node_override() {
     ref=""
   fi
 
-  #TODO: remove this workaround as soon as https://github.com/nix-community/poetry2nix/pull/1368 is merged
-  poetry2nix_ref=("--override-input" "poetry2nix" "github:nix-community/poetry2nix/026032a8b4e6e948c608de0fc559c6d0256e6117")
-
-  echo --override-input cardano-node "github:input-output-hk/cardano-node$ref" "${poetry2nix_ref[@]}" --recreate-lock-file
+  echo --override-input cardano-node "github:input-output-hk/cardano-node$ref" --recreate-lock-file
 }
