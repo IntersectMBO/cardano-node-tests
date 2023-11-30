7b63a76b1057747ab73140218ae9a65e074b5be3
Artur Wieczorek <artur.wieczorek@iohk.io>
2023-11-30 20:15:27 +0100

    Update cabal tests

--- cabal_build_tests/install-node.sh
+++ cabal_build_tests/install-node.sh
@@ -7,10 +7,11 @@
 # Please note: 'source ~/.bashrc' cmd is not used because Docker runs this script as subscript
 
 # Versions
-GHC_VERSION="8.10.7"
-CABAL_VERSION="3.6.2.0"
+GHC_VERSION="9.6.3"
+CABAL_VERSION="3.10.2.0"
 LIBSODIUM_VERSION="dbb48cc"
 SECP256K1_VERSION="ac83be33"
+BLST_VERSION="v0.3.10"
 
 echo ""
 
@@ -43,42 +44,34 @@ else
   echo "'PATH' already contains ~/.local/bin"
 fi
 
-# Set up GHC and cabal-install versions
-GHC="ghc-${GHC_VERSION}"
-GHC_SUFFIX="x86_64-deb10-linux.tar.xz"
-GHC_FULL="${GHC}-${GHC_SUFFIX}"
-
-echo "Using GHC version $GHC_VERSION"
-
-CABAL="cabal-install-${CABAL_VERSION}"
-CABAL_SUFFIX="x86_64-linux-deb10.tar.xz"
-CABAL_FULL="${CABAL}-${CABAL_SUFFIX}"
-
-echo "Using cabal-install version '$CABAL_VERSION'"
-
 # Install dependencies
 echo "Install dependencies"
 if [[ "$(</etc/os-release)" == *"fedora"* ]]; then
   echo "Running on Fedora"
   yum update -y
-  yum install git gcc gcc-c++ tmux gmp-devel make tar xz wget zlib-devel libtool autoconf -y
-  yum install systemd-devel ncurses-devel ncurses-compat-libs which jq openssl-devel lmdb-devel -y
+  yum install curl systemd-devel ncurses-devel ncurses-compat-libs which jq openssl-devel lmdb-devel -y
 elif [[ "$(</etc/os-release)" == *"ubuntu"* ]]; then
   echo "Running on Ubuntu"
   apt-get update -y
-  apt-get install automake build-essential pkg-config libffi-dev libgmp-dev libssl-dev libtinfo-dev libsystemd-dev zlib1g-dev make g++ tmux git jq wget libncursesw5 libtool autoconf liblmdb-dev -y
+  apt-get install curl automake build-essential pkg-config libffi-dev libgmp-dev libssl-dev libtinfo-dev libsystemd-dev zlib1g-dev make g++ tmux git jq wget libncursesw5 libtool autoconf liblmdb-dev -y
 else
   >&2 echo "/etc/os-relase does not contain 'fedora' or 'ubuntu'"
   >&2 cat /etc/os-release
   exit 1
 fi
 
+# Install GHCup - the main installer for Haskell
+echo "Install GHCup"
+curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | BOOTSTRAP_HASKELL_NONINTERACTIVE=1 BOOTSTRAP_HASKELL_MINIMAL=1 BOOTSTRAP_HASKELL_ADJUST_BASHRC=P sh
+
+# Source changes in order to use GHCup in current terminal session
+# shellcheck source=/dev/null
+source /root/.ghcup/env
+
 # Download, unpack, install and update Cabal
 echo "Download, unpack, install and update Cabal"
-wget "https://downloads.haskell.org/~cabal/${CABAL}/${CABAL_FULL}"
-tar -xf "$CABAL_FULL"
-rm "$CABAL_FULL"
-mv cabal ~/.local/bin/
+ghcup install cabal $CABAL_VERSION
+ghcup set cabal $CABAL_VERSION
 
 cabal update
 
@@ -91,15 +84,9 @@ fi
 
 # Download, unpack and install GHC
 echo "Download and install GHC"
+ghcup install ghc $GHC_VERSION
+ghcup set ghc $GHC_VERSION
 
-mkdir -p ~/src || exit 1
-cd ~/src || exit 1
-wget "https://downloads.haskell.org/ghc/${GHC_VERSION}/${GHC_FULL}"
-tar -xf "$GHC_FULL"
-rm "$GHC_FULL"
-cd "$GHC" || exit 1
-./configure
-make install
 
 if [[ $(ghc --version) != *"version $GHC_VERSION"* ]]; then
   >&2 echo "ghc version '$(ghc --version)' is not '$GHC_VERSION'"
@@ -123,6 +110,8 @@ make install
 
 export LD_LIBRARY_PATH="/usr/local/lib:${LD_LIBRARY_PATH}"
 export PKG_CONFIG_PATH="/usr/local/lib/pkgconfig:${PKG_CONFIG_PATH}"
+# shellcheck source=/dev/null
+source ~/.bashrc
 
 # Install Secp256k1
 echo "Install Secp256k1"
@@ -136,6 +125,33 @@ git checkout "$SECP256K1_VERSION"
 ./configure --enable-module-schnorrsig --enable-experimental
 make
 make install
+
+# Install BLST
+echo "Install BLST"
+
+mkdir -p ~/src || exit 1
+cd ~/src || exit 1
+git clone https://github.com/supranational/blst
+cd blst || exit 1
+git checkout "$BLST_VERSION"
+./build.sh
+cat > libblst.pc << EOF
+prefix=/usr/local
+exec_prefix=\${prefix}
+libdir=\${exec_prefix}/lib
+includedir=\${prefix}/include
+
+Name: libblst
+Description: Multilingual BLS12-381 signature library
+URL: https://github.com/supranational/blst
+Version: 0.3.10
+Cflags: -I\${includedir}
+Libs: -L\${libdir} -lblst
+EOF
+cp libblst.pc /usr/local/lib/pkgconfig/
+cp bindings/blst_aux.h bindings/blst.h bindings/blst.hpp  /usr/local/include/
+cp libblst.a /usr/local/lib
+chmod u=rw,go=r /usr/local/{lib/{libblst.a,pkgconfig/libblst.pc},include/{blst.{h,hpp},blst_aux.h}}
 
 # Download the source code for cardano-node
 echo "Download the source code for cardano-node"
@@ -168,15 +184,17 @@ case "$GIT_OBJECT_TYPE" in
 esac
 
 git checkout "$GIT_OBJECT"
+GIT_REV=$(git rev-parse HEAD)
 
 # Configure build options
 echo "Configure build options"
 echo "with-compiler: ghc-${GHC_VERSION}" >> cabal.project.local
 
-# Build and install the node
-echo "Build and install the node"
+# Build and install the node and cli
+echo "Build and install the node and cli"
 cabal update
 cabal build all
+cabal build cardano-cli
 
 cp -p "$(./scripts/bin-path.sh cardano-node)" ~/.local/bin/
 cp -p "$(./scripts/bin-path.sh cardano-cli)" ~/.local/bin/
@@ -184,35 +202,38 @@ cp -p "$(./scripts/bin-path.sh cardano-cli)" ~/.local/bin/
 # Verify installation
 case "$GIT_OBJECT_TYPE" in
   tag)
-    GIT_TAG="$GIT_OBJECT"
+    CARDANO_NODE_VERSION="$GIT_OBJECT"
+
+    if [[ "${CARDANO_NODE_VERSION:0-4}" == "-pre" ]]; then
+      CARDANO_NODE_VERSION="${GIT_OBJECT:0:-4}"
+    fi
 
     echo "Verify 'cardano-cli' is installed"
-    if [[ "$(cardano-cli --version)" == *"$GIT_TAG"* ]]; then
+    if [[ "$(cardano-cli --version | tail -n 1)" == *"$GIT_REV"* ]]; then
       cardano-cli --version
     else
-      >&2 echo "'$(cardano-cli --version)' does not contain '$GIT_TAG'"
+      >&2 echo "'$(cardano-cli --version)' does not contain '$GIT_REV'"
       exit 1
     fi
 
     echo "Verify 'cardano-node' is installed"
-    if [[ "$(cardano-node --version)" == *"$GIT_TAG"* ]]; then
+    if [[ "$(cardano-node --version)" == *"$CARDANO_NODE_VERSION"* ]]; then
       cardano-node --version
     else
-      >&2 echo "'$(cardano-node --version)' does not contain '$GIT_TAG'"
+      >&2 echo "'$(cardano-node --version)' does not contain '$CARDANO_NODE_VERSION'"
       exit 1
     fi
     ;;
   commit)
     echo "Verify 'cardano-cli' is installed"
-    if ! cardano-cli --version; then
-      >&2 echo "'cardano-cli --version' failed"
+    if ! [[ "$(cardano-cli --version | tail -n 1)" == *"$GIT_REV"* ]]; then
+      >&2 echo "'$(cardano-cli --version)' failed"
       exit 1
     fi
 
-    # Verify node is installed
     echo "Verify 'cardano-node' is installed"
-    if ! cardano-node --version; then
-      >&2 echo "'cardano-node --version' failed"
+    if ! [[ "$(cardano-node --version | tail -n 1)" == *"$GIT_REV"* ]]; then
+      >&2 echo "'$(cardano-node --version)' failed"
       exit 1
     fi
     ;;
