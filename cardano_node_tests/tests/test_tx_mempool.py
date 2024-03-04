fcc96c7f4747e32d1f53df5d8446c4770e8a01e4
Martin Kourim <martin.kourim@iohk.io>
2024-03-04 10:03:17 +0100

    Handle Txs that are stuck in mempool

--- cardano_node_tests/tests/test_tx_mempool.py
+++ cardano_node_tests/tests/test_tx_mempool.py
@@ -55,6 +55,15 @@ class TestMempool:
         cluster = cluster_singleton
         temp_template = common.get_test_id(cluster)
         amount = 2_000_000
+        has_query_mempool = clusterlib_utils.cli_has("query tx-mempool")
+
+        init_next_txid = ""
+        if has_query_mempool:
+            # Make sure existing Txs are removed from mempool before submitting the new Tx
+            cluster.wait_for_new_block(new_blocks=2)
+            # Get the current `nextTx` before submitting the new Tx. There can be an earlier Tx
+            # that is still "stuck" in mempool.
+            init_next_txid = cluster.g_query.get_mempool_next_tx().get("nextTx") or ""
 
         src_addr = payment_addrs_locked[0]
         dst_addr = payment_addrs_locked[1]
@@ -83,8 +92,8 @@ class TestMempool:
 
         txid = cluster.g_transaction.get_txid(tx_body_file=tx_raw_output.out_file)
 
-        # we want to submit the Tx and then re-submit it and see the expected error, to make sure
-        # the Tx made it to mempool
+        # We want to submit the Tx and then re-submit it and see the expected error, to make sure
+        # the Tx made it to mempool.
         for r in range(5):
             try:
                 cluster.g_transaction.submit_tx_bare(tx_file=out_file_signed)
@@ -95,17 +104,17 @@ class TestMempool:
         else:
             raise AssertionError("Failed to make sure the Tx is in mempool")
 
-        # if the Tx is only in mempool, its txins can still be queried
+        # If the Tx is only in mempool, its txins can still be queried
         txin_queried = bool(cluster.g_query.get_utxo(utxo=tx_raw_output.txins[0]))
 
         if not txin_queried:
             pytest.skip("the Tx was removed from mempool before running `query utxo`")
 
         def _check_query_mempool() -> None:
-            if not clusterlib_utils.cli_has("query tx-mempool"):
+            if not has_query_mempool:
                 return
 
-            # check 'query tx-mempool next-tx'
+            # Check 'query tx-mempool next-tx'
             mempool_next_tx = cluster.g_query.get_mempool_next_tx()
             next_txid = mempool_next_tx["nextTx"]
             if next_txid is None:
@@ -113,11 +122,12 @@ class TestMempool:
                     "The Tx was removed from mempool before running `query tx-mempool next-tx`"
                 )
                 return
-            assert (
-                next_txid == txid
+            assert next_txid in (
+                txid,
+                init_next_txid,
             ), f"The reported nextTx is '{mempool_next_tx['nextTx']}', expected '{txid}'"
 
-            # check 'query tx-mempool exists <TxId>'
+            # Check 'query tx-mempool exists <TxId>'
             txid_exists_in_mempool = cluster.g_query.get_mempool_tx_exists(txid=txid)
             if not txid_exists_in_mempool["exists"]:
                 LOGGER.warning(
@@ -128,7 +138,7 @@ class TestMempool:
                 txid_exists_in_mempool["txId"] == txid
             ), f"The reported txId is '{txid_exists_in_mempool['txId']}', expected '{txid}'"
 
-            # check that the slot reported by the tx-mempool commands is correct
+            # Check that the slot reported by the tx-mempool commands is correct
             assert mempool_next_tx["slot"] == txid_exists_in_mempool["slot"], (
                 f"The slots reported by the 'tx-mempool' commands don't match: "
                 f"{mempool_next_tx['slot']} vs {txid_exists_in_mempool['slot']}"
@@ -136,5 +146,5 @@ class TestMempool:
 
         _check_query_mempool()
 
-        # make sure the txin is removed from mempool so it cannot be selected by other tests
+        # Make sure the txin is removed from mempool so it cannot be selected by other tests
         cluster.wait_for_new_block(new_blocks=2)
