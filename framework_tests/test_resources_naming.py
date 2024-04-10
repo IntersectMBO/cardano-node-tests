12d4b276b90fbf40012653e2775d4bbcd9590c34
Martin Kourim <martin.kourim@iohk.io>
2024-04-10 22:09:16 +0200

    Refactor resource names sanitization

--- framework_tests/test_resources_naming.py
+++ framework_tests/test_resources_naming.py
@@ -1,21 +1,21 @@
-from cardano_node_tests.cluster_management import cluster_getter
+from cardano_node_tests.cluster_management import resources
 from cardano_node_tests.cluster_management import resources_management
 
 ALL_POOLS = {"pool1", "pool2", "pool3", "pool4", "pool5"}
 
 
-def test_sanitize4path():
+def test_sanitize_res_name():
     oneof = resources_management.OneOf(ALL_POOLS)
     lock_resources = ["$$res", oneof, "res1"]
     use_resources = ["res2", oneof, "res$&#@"]
-    mark = cluster_getter.sanitize4path(
+    mark = resources.sanitize_res_name(
         "res-Foo@Bar@res@Foo@Bar@res@Foo@Bar@res@Foo@Bar@res@Foo@Bar@res@Foo@Bar@res@Foo@Bar@"
     )
     lock_resources = [
-        cluster_getter.sanitize4path(r) if isinstance(r, str) else r for r in lock_resources
+        resources.sanitize_res_name(r) if isinstance(r, str) else r for r in lock_resources
     ]
     use_resources = [
-        cluster_getter.sanitize4path(r) if isinstance(r, str) else r for r in use_resources
+        resources.sanitize_res_name(r) if isinstance(r, str) else r for r in use_resources
     ]
     assert mark == "res-Foo_Bar_res_Foo_"
     assert lock_resources == ["_res", oneof, "res1"]
@@ -23,5 +23,5 @@ def test_sanitize4path():
 
 
 def test_get_unsanitized():
-    unsanitized = cluster_getter.get_unsanitized(["res1", "res2", "res$3", "res#%4"])
+    unsanitized = resources.get_unsanitized(["res1", "res2", "res$3", "res#%4"])
     assert unsanitized == ["res$3", "res#%4"]
