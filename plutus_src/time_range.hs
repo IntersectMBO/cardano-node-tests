dc91a381e2aea61543b7a592c10ba964be9a0c94
Martin Kourim <martin.kourim@iohk.io>
2024-06-03 16:45:37 +0200

    Delete plutus_src dir
    It was replaced with https://github.com/mkoura/plutus-scripts-e2e

--- plutus_src/time_range.hs
+++
@@ -1,8 +0,0 @@
-mkPolicy :: POSIXTime -> ScriptContext -> Bool
-mkPolicy dl ctx = (to dl) `contains` range
-  where
-    info :: TxInfo
-    info = scriptContextTxInfo ctx
-
-    range :: POSIXTimeRange
-    range = txInfoValidRange info
