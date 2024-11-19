f91a0aba192ba87f083fa988ee16a2778ac77705
Martin Kourim <martin.kourim@iohk.io>
2024-11-19 12:46:50 +0100

    feat: improve stop_cluster_instances.sh script
    - Add echo statement to indicate stopping of cluster instances
    - Suppress error messages from supervisord_stop script

--- .github/stop_cluster_instances.sh
+++ .github/stop_cluster_instances.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
-# stop all running cluster instances
 
 stop_instances() {
+  echo "Stopping all running cluster instances"
+
   local workdir="${1:?}"
   for sc in "$workdir"/state-cluster*; do
     [ -d "$sc" ] || continue
-    "$sc/supervisord_stop" || true
+    "$sc/supervisord_stop" 2>/dev/null || :
   done
 }
