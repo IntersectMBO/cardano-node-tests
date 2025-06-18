99c974917938ecf771f63864924a93603a546b12
Martin Kourim <martin.kourim@iohk.io>
2025-06-18 16:19:16 +0200

    refactor(ci): rename PATH_APPEND to PATH_PREPEND
    Renamed the environment variable PATH_APPEND to PATH_PREPEND in all CI
    scripts to better reflect its usage as a path prefix. Updated all
    references and export statements accordingly for consistency and clarity.
    No functional changes introduced.

--- .github/source_plutus_apps.sh
+++ .github/source_plutus_apps.sh
@@ -16,9 +16,9 @@ nix build \
   -o create-script-context-build || exit 1
 [ -e create-script-context-build/bin/create-script-context ] || exit 1
 
-# Add `create-script-context` to PATH_APPEND
-PATH_APPEND="${PATH_APPEND:+"${PATH_APPEND}:"}$(readlink -m create-script-context-build/bin)"
-export PATH_APPEND
+# Add `create-script-context` to PATH_PREPEND
+PATH_PREPEND="${PATH_PREPEND:+"${PATH_PREPEND}:"}$(readlink -m create-script-context-build/bin)"
+export PATH_PREPEND
 
 cd "$_origpwd" || exit 1
 unset _origpwd
