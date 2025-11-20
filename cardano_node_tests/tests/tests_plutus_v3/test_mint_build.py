ca819d52d1984e1a51cbfbd83bd59364ee042b9c
kwxm <kenneth.mackenzie@iohk.io>
2025-11-20 04:51:24 +0000

    End-to-end tests for dropList builtin

--- cardano_node_tests/tests/tests_plutus_v3/test_mint_build.py
+++ cardano_node_tests/tests/tests_plutus_v3/test_mint_build.py
@@ -212,3 +212,143 @@ class TestPlutusBatch5V3Builtins:
                     plutus_v_record=script,
                     success_expected=False,
                 )
+
+
+# Just a slightly modified copy of TestPlutusBatch5V3Builtins
+class TestPlutusBatch6V3Builtins:
+    """Tests for batch6 of Plutus Core built-in functions."""
+
+    success_scripts = plutus_common.SUCCEEDING_MINTING_BATCH6_SCRIPTS_V3
+
+    fail_scripts = plutus_common.FAILING_MINTING_BATCH6_SCRIPTS_V3
+
+    @pytest.fixture
+    def payment_addrs(
+        self,
+        update_cost_model: None,  # noqa: ARG002
+        cluster_manager: cluster_management.ClusterManager,
+        cluster: clusterlib.ClusterLib,
+    ) -> list[clusterlib.AddressRecord]:
+        """Create new payment address."""
+        addrs = common.get_payment_addrs(
+            name_template=common.get_test_id(cluster),
+            cluster_manager=cluster_manager,
+            cluster_obj=cluster,
+            num=2,
+            fund_idx=[0],
+            amount=700_000_000,
+        )
+        return addrs
+
+    def run_scenario(
+        self,
+        cluster_obj: clusterlib.ClusterLib,
+        payment_addrs: list[clusterlib.AddressRecord],
+        plutus_v_record: plutus_common.PlutusScriptData,
+        success_expected: bool,
+    ):
+        """Run an e2e test for a Plutus builtin."""
+        temp_template = common.get_test_id(cluster_obj)
+
+        payment_addr = payment_addrs[0]
+        issuer_addr = payment_addrs[1]
+
+        lovelace_amount = 2_000_000
+        token_amount = 5
+        script_fund = 10_000_000
+
+        minting_cost = plutus_common.compute_cost(
+            execution_cost=plutus_v_record.execution_cost,
+            protocol_params=cluster_obj.g_query.get_protocol_params(),
+        )
+
+        # Step 1: fund the token issuer and create UTXO for collaterals
+
+        mint_utxos, collateral_utxos, _tx_output_step1 = mint_build._fund_issuer(
+            cluster_obj=cluster_obj,
+            temp_template=temp_template,
+            payment_addr=payment_addr,
+            issuer_addr=issuer_addr,
+            minting_cost=minting_cost,
+            amount=script_fund,
+        )
+
+        # Step 2: mint the "qacoin"
+
+        policyid = cluster_obj.g_transaction.get_policyid(plutus_v_record.script_file)
+        asset_name = f"qacoin{clusterlib.get_rand_str(4)}".encode().hex()
+        token = f"{policyid}.{asset_name}"
+        mint_txouts = [
+            clusterlib.TxOut(address=issuer_addr.address, amount=token_amount, coin=token)
+        ]
+
+        plutus_mint_data = [
+            clusterlib.Mint(
+                txouts=mint_txouts,
+                script_file=plutus_v_record.script_file,
+                collaterals=collateral_utxos,
+                redeemer_file=plutus_common.REDEEMER_42,
+            )
+        ]
+
+        tx_files_step2 = clusterlib.TxFiles(
+            signing_key_files=[issuer_addr.skey_file],
+        )
+        txouts_step2 = [
+            clusterlib.TxOut(address=issuer_addr.address, amount=lovelace_amount),
+            *mint_txouts,
+        ]
+
+        try:
+            tx_output_step2 = clusterlib_utils.build_and_submit_tx(
+                cluster_obj=cluster_obj,
+                name_template=f"{temp_template}_step2",
+                src_address=payment_addr.address,
+                build_method=clusterlib_utils.BuildMethods.BUILD,
+                tx_files=tx_files_step2,
+                txins=mint_utxos,
+                txouts=txouts_step2,
+                mint=plutus_mint_data,
+            )
+        except clusterlib.CLIError as excp:
+            if success_expected:
+                raise
+            if "The machine terminated because of an error" in str(excp):
+                return
+            raise
+
+        out_utxos = cluster_obj.g_query.get_utxo(tx_raw_output=tx_output_step2)
+        token_utxo = clusterlib.filter_utxos(
+            utxos=out_utxos, address=issuer_addr.address, coin=token
+        )
+        assert token_utxo and token_utxo[0].amount == token_amount, "The token was not minted"
+
+    @allure.link(helpers.get_vcs_link())
+    @pytest.mark.long
+    @pytest.mark.team_plutus
+    @pytest.mark.upgrade_step3
+    def test_plutusv3_batch6_builtins(
+        self,
+        update_cost_model: None,  # noqa: ARG002
+        cluster: clusterlib.ClusterLib,
+        payment_addrs: list[clusterlib.AddressRecord],
+        subtests: pytest_subtests.SubTests,
+    ):
+        """Test minting with the batch6 of Plutus Core built-in functions."""
+        for script in self.success_scripts:
+            with subtests.test(script=script.script_file.stem):
+                self.run_scenario(
+                    cluster_obj=cluster,
+                    payment_addrs=payment_addrs,
+                    plutus_v_record=script,
+                    success_expected=True,
+                )
+
+        for script in self.fail_scripts:
+            with subtests.test(script=script.script_file.stem):
+                self.run_scenario(
+                    cluster_obj=cluster,
+                    payment_addrs=payment_addrs,
+                    plutus_v_record=script,
+                    success_expected=False,
+                )
