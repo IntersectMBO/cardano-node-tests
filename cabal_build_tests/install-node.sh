ce95f1ca8f809b4cfa325830ee69212cf0d84d6b
Martin Kourim <martin.kourim@iohk.io>
2025-11-21 16:47:52 +0100

    feat(cabal-tests): Update for building cardano-node 10.6.0

--- cabal_build_tests/install-node.sh
+++ cabal_build_tests/install-node.sh
@@ -3,6 +3,9 @@
 # Builds 'cardano-node' and 'cardano-cli' on Ubuntu or Fedora using Cabal and verifies
 # successful installation.
 #
+# Based on instructions from
+# <https://developers.cardano.org/docs/get-started/infrastructure/node/installing-cardano-node/#building-via-cabal>
+#
 # Please note: sudo is not used because user is root
 # Please note: 'source ~/.bashrc' cmd is not used because Docker runs this script as subscript
 
@@ -12,23 +15,12 @@ CABAL_VERSION="3.12.1.0"
 
 echo ""
 
-# Check that environment variables are correctly set up.
-case "$GIT_OBJECT_TYPE" in
-  tag | commit)
-    :
-    ;;
-  *)
-    >&2 printf "Please specify 'GIT_OBJECT_TYPE' on docker run.\ne.g. '-e GIT_OBJECT_TYPE=tag' or '-e GIT_OBJECT_TYPE=commit'"
-    exit 1
-    ;;
-esac
-
 if [[ -z "${GIT_OBJECT}" ]]; then
   >&2 printf "Please specify 'GIT_OBJECT' on docker run.\ne.g. '-e GIT_OBJECT=8.0.0' for tags, or '-e GIT_OBJECT=78wagy3aw87ef' for commits."
   exit 1
 fi
 
-echo "Using git object '$GIT_OBJECT' of type '$GIT_OBJECT_TYPE'"
+echo "Using git object '$GIT_OBJECT'"
 
 cd ~ || exit 1
 
@@ -47,26 +39,21 @@ echo "Install dependencies"
 if [[ "$(</etc/os-release)" == *"fedora"* ]]; then
   echo "Running on Fedora"
   yum update -y
-  yum install curl systemd-devel ncurses-devel ncurses-compat-libs which jq openssl-devel lmdb-devel -y
+  yum install git gcc gcc-c++ tmux gmp-devel make tar xz wget zlib-devel libtool autoconf -y
+  yum install systemd-devel ncurses-devel ncurses-compat-libs which jq openssl-devel lmdb-devel -y
 elif [[ "$(</etc/os-release)" == *"ubuntu"* ]]; then
   echo "Running on Ubuntu"
   apt-get update -y
-  apt-get install -y curl automake build-essential pkg-config libffi-dev libgmp-dev libssl-dev libncurses-dev libsystemd-dev zlib1g-dev make g++ tmux git jq wget libtool autoconf liblmdb-dev
+  apt-get install automake build-essential pkg-config libffi-dev libgmp-dev libssl-dev libncurses-dev libsystemd-dev zlib1g-dev make g++ tmux git jq wget libtool autoconf liblmdb-dev -y
 else
   >&2 echo "/etc/os-relase does not contain 'fedora' or 'ubuntu'"
   >&2 cat /etc/os-release
   exit 1
 fi
 
-# Versions of libraries
+# Version of iohk-nix
 IOHKNIX_VERSION="$(curl "https://raw.githubusercontent.com/IntersectMBO/cardano-node/$GIT_OBJECT/flake.lock" | jq -r '.nodes.iohkNix.locked.rev')"
 echo "iohk-nix version: $IOHKNIX_VERSION"
-LIBSODIUM_VERSION="$(curl "https://raw.githubusercontent.com/input-output-hk/iohk-nix/$IOHKNIX_VERSION/flake.lock" | jq -r '.nodes.sodium.original.rev')"
-echo "Using sodium version: $LIBSODIUM_VERSION"
-SECP256K1_VERSION="$(curl "https://raw.githubusercontent.com/input-output-hk/iohk-nix/$IOHKNIX_VERSION/flake.lock" | jq -r '.nodes.secp256k1.original.ref')"
-echo "Using secp256k1 version: ${SECP256K1_VERSION}"
-BLST_VERSION="$(curl "https://raw.githubusercontent.com/input-output-hk/iohk-nix/$IOHKNIX_VERSION/flake.lock" | jq -r '.nodes.blst.original.ref')"
-echo "Using blst version: ${BLST_VERSION}"
 
 # Install GHCup - the main installer for Haskell
 echo "Install GHCup"
@@ -77,11 +64,9 @@ curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | BOOTSTRAP_H
 source /root/.ghcup/env
 
 # Download, unpack, install and update Cabal
-echo "Download, unpack, install and update Cabal"
-ghcup install cabal $CABAL_VERSION
-ghcup set cabal $CABAL_VERSION
-
-cabal update
+echo "Download, unpack, install Cabal and GHC"
+ghcup install ghc $GHC_VERSION --set
+ghcup install cabal $CABAL_VERSION --set
 
 if [[ "$(cabal --version)" != *"cabal-install version $CABAL_VERSION"* ]]; then
   >&2 echo "cabal version '$(cabal --version)' is not '$CABAL_VERSION'"
@@ -89,12 +74,6 @@ if [[ "$(cabal --version)" != *"cabal-install version $CABAL_VERSION"* ]]; then
 else
   echo "cabal version '$CABAL_VERSION' is correct"
 fi
-
-# Download, unpack and install GHC
-echo "Download and install GHC"
-ghcup install ghc $GHC_VERSION
-ghcup set ghc $GHC_VERSION
-
 
 if [[ $(ghc --version) != *"version $GHC_VERSION"* ]]; then
   >&2 echo "ghc version '$(ghc --version)' is not '$GHC_VERSION'"
@@ -108,12 +87,17 @@ echo "Install Libsodium"
 
 mkdir -p ~/src || exit 1
 cd ~/src || exit 1
+
+LIBSODIUM_VERSION="$(curl "https://raw.githubusercontent.com/input-output-hk/iohk-nix/$IOHKNIX_VERSION/flake.lock" | jq -r '.nodes.sodium.original.rev')"
+echo "Using sodium version: $LIBSODIUM_VERSION"
+
 git clone https://github.com/IntersectMBO/libsodium
 cd libsodium || exit 1
 git checkout "$LIBSODIUM_VERSION"
 ./autogen.sh
 ./configure
 make
+make check
 make install
 
 export LD_LIBRARY_PATH="/usr/local/lib:${LD_LIBRARY_PATH}"
@@ -126,12 +110,16 @@ echo "Install Secp256k1"
 
 mkdir -p ~/src || exit 1
 cd ~/src || exit 1
-git clone https://github.com/bitcoin-core/secp256k1
+
+SECP256K1_VERSION="$(curl "https://raw.githubusercontent.com/input-output-hk/iohk-nix/$IOHKNIX_VERSION/flake.lock" | jq -r '.nodes.secp256k1.original.ref')"
+echo "Using secp256k1 version: ${SECP256K1_VERSION}"
+
+git clone --depth 1 --branch "${SECP256K1_VERSION}" https://github.com/bitcoin-core/secp256k1
 cd secp256k1 || exit 1
-git checkout "$SECP256K1_VERSION"
 ./autogen.sh
 ./configure --enable-module-schnorrsig --enable-experimental
 make
+make check
 make install
 
 # Install BLST
@@ -139,9 +127,12 @@ echo "Install BLST"
 
 mkdir -p ~/src || exit 1
 cd ~/src || exit 1
-git clone https://github.com/supranational/blst
+
+BLST_VERSION="$(curl "https://raw.githubusercontent.com/input-output-hk/iohk-nix/$IOHKNIX_VERSION/flake.lock" | jq -r '.nodes.blst.original.ref')"
+echo "Using blst version: ${BLST_VERSION}"
+
+git clone --depth 1 --branch "${BLST_VERSION}" https://github.com/supranational/blst
 cd blst || exit 1
-git checkout "$BLST_VERSION"
 ./build.sh
 cat > libblst.pc << EOF
 prefix=/usr/local
@@ -152,7 +143,7 @@ includedir=\${prefix}/include
 Name: libblst
 Description: Multilingual BLS12-381 signature library
 URL: https://github.com/supranational/blst
-Version: ${BLST_VERSION}
+Version: ${BLST_VERSION#v}
 Cflags: -I\${includedir}
 Libs: -L\${libdir} -lblst
 EOF
@@ -177,21 +168,6 @@ if [[ "$OBJ_TYPE" != "commit" && "$OBJ_TYPE" != "tag" ]]; then
   exit 1
 fi
 
-case "$GIT_OBJECT_TYPE" in
-  tag)
-    if [[ "$(git tag)" != *"$GIT_OBJECT"* ]]; then
-      >&2 echo "'$(git tag)' does not contain '$GIT_OBJECT'"
-      exit 1
-    fi
-    ;;
-  commit)
-    :
-    ;;
-  *)
-    exit 1
-    ;;
-esac
-
 git checkout "$GIT_OBJECT"
 GIT_REV=$(git rev-parse HEAD)
 
@@ -202,56 +178,26 @@ echo "with-compiler: ghc-${GHC_VERSION}" >> cabal.project.local
 # Build and install the node and cli
 echo "Build and install the node and cli"
 cabal update
-cabal build all
+cabal build exe:cardano-node
 cabal build cardano-cli
 
-cp -p "$(./scripts/bin-path.sh cardano-node)" ~/.local/bin/
-cp -p "$(./scripts/bin-path.sh cardano-cli)" ~/.local/bin/
+cp -p "$(cabal list-bin cardano-node)" ~/.local/bin/
+cp -p "$(cabal list-bin cardano-cli)" ~/.local/bin/
 
 # Verify installation
-case "$GIT_OBJECT_TYPE" in
-  tag)
-    CARDANO_NODE_VERSION="$GIT_OBJECT"
+echo "Verify 'cardano-cli' is installed"
+if ! [[ "$(cardano-cli --version | tail -n 1)" == *"$GIT_REV"* ]]; then
+  >&2 echo "'$(cardano-cli --version)' failed"
+  exit 1
+fi
 
-    if [[ "${CARDANO_NODE_VERSION:0-4}" == "-pre" ]]; then
-      CARDANO_NODE_VERSION="${GIT_OBJECT:0:-4}"
-    fi
+echo "Verify 'cardano-node' is installed"
+if ! [[ "$(cardano-node --version | tail -n 1)" == *"$GIT_REV"* ]]; then
+  >&2 echo "'$(cardano-node --version)' failed"
+  exit 1
+fi
 
-    echo "Verify 'cardano-cli' is installed"
-    if [[ "$(cardano-cli --version | tail -n 1)" == *"$GIT_REV"* ]]; then
-      cardano-cli --version
-    else
-      >&2 echo "'$(cardano-cli --version)' does not contain '$GIT_REV'"
-      exit 1
-    fi
-
-    echo "Verify 'cardano-node' is installed"
-    if [[ "$(cardano-node --version)" == *"$CARDANO_NODE_VERSION"* ]]; then
-      cardano-node --version
-    else
-      >&2 echo "'$(cardano-node --version)' does not contain '$CARDANO_NODE_VERSION'"
-      exit 1
-    fi
-    ;;
-  commit)
-    echo "Verify 'cardano-cli' is installed"
-    if ! [[ "$(cardano-cli --version | tail -n 1)" == *"$GIT_REV"* ]]; then
-      >&2 echo "'$(cardano-cli --version)' failed"
-      exit 1
-    fi
-
-    echo "Verify 'cardano-node' is installed"
-    if ! [[ "$(cardano-node --version | tail -n 1)" == *"$GIT_REV"* ]]; then
-      >&2 echo "'$(cardano-node --version)' failed"
-      exit 1
-    fi
-    ;;
-  *)
-    exit 1
-    ;;
-esac
-
-# Succes message
+# Success message
 printf "\nSuccess\n\n"
 echo "You can use 'docker exec -it <container-id> /bin/bash' in a separate session to play in the environment."
 echo "Press any key to stop the container."
